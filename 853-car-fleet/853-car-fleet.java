1ab7079376076713ca199946d147d0a526edf51c
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-02-15 11:42:16 +0530

    Time: 76 ms (83.59%), Space: 52.5 MB (79.56%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.java
@@ -0,0 +1,43 @@
+class Solution {
+    // O(NlogN + N) O(2N)
+    /*
+    Sort based on positon
+    Start from last, see if current car can catchup with the car ahead, before they reach end
+    if it can't increase no of car fleets
+    also keep updating the speed of the car ahead at each point
+    */
+    public int carFleet(int target, int[] position, int[] speed) { 
+        int ans = 1, n = position.length;
+        car [] cars = new car[n];
+        for (int i = 0; i<n; i++) {
+            cars[i] = new car(position[i], speed[i]);
+        }
+        Arrays.sort(cars);
+        car lst = null;
+        for (int i = n-1; i>=0; i--) {
+            if (lst==null) lst = cars[i];
+            else {
+                if (!catchup(lst, cars[i], target)) {
+                    lst = cars[i];
+                    ans++;
+                }
+            }
+        }      
+        return ans;
+    }
+    // true if second car can catch up to first car
+    public boolean catchup(car fir, car sec, int dest) {
+        double t = (dest-fir.pos)/fir.v;
+        if (sec.pos + t*sec.v >= dest) {
+            return true;
+        }
+        else return false;
+    }
+    public class car implements Comparable<car>{
+        double pos, v;
+        car(int i, int speed) { pos = (double) i; v = speed; }
+        public int compareTo(car nxt) {
+            return Double.compare(pos, nxt.pos);
+        }
+    }
+}
