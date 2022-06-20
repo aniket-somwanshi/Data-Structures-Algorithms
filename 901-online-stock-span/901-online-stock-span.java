57646c1dfc975db72b6bb58462f6312c7cc2e66b
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-06-20 22:46:12 +0530

    Time: 108 ms (8.79%), Space: 72.1 MB (52.77%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.java
+++ 901-online-stock-span/901-online-stock-span.java
@@ -12,7 +12,7 @@ class StockSpanner {
         stack = new Stack<>();
     }
     
-    // O(N) O(N) -- monotonous stack
+    // O(N) O(N) -- monotoic stack
     public int next(int price) {
         int currentSpan = 1;
         while (stack.isEmpty() == false && stack.peek().price <= price) {
