6a4f58e96be01d91fff37415fff8ff1269fc1c4f
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-06-22 12:20:15 +0530

    Time: 2 ms (30.36%), Space: 43.3 MB (71.86%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public int minCostClimbingStairs(int[] cost) 
+    {
+        // O(n) time | O(n) space
+        // we hyave [cost.length + 1] because we treat top floor as reachable stair
+        int minCost[] = new int[cost.length + 1];
+        
+        // we can directly start from step 2 because we minimum cost to step 0 or 1 are both 0
+        for(int i = 2; i < minCost.length; i++)
+        {
+            int takeOneStep = minCost[i-1] + cost[i-1];
+            int takeTwoStep = minCost[i-2] + cost[i-2];
+            minCost[i] = Math.min(takeOneStep, takeTwoStep);
+        }
+        return minCost[minCost.length - 1];
+    }
+}
