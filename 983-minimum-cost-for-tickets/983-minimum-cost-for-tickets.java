cb321fd612eb9c545b73bc28d617c69468413286
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-07-19 17:35:57 +0530

    Time: 53 ms (5.01%), Space: 42.5 MB (34.42%) - LeetHub

---
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
@@ -0,0 +1,42 @@
+class Solution {
+    private Integer[] minCost; // min cost from index i to end
+    private int[] days;
+    private int[] costs;
+    public int mincostTickets(int[] days, int[] costs) {
+        this.days = days;
+        this.costs = costs;
+        this.minCost = new Integer[days.length];
+        return dfs(0);
+    }
+    
+    private int dfs(int index) {
+        if (index >= days.length) return 0;
+        
+        if (minCost[index] != null) return minCost[index];
+        
+        int cost1 = dfs(getLastIndexCovered(index, 1) + 1) + costs[0];  // 1 day pass taken 
+        int cost2 = dfs(getLastIndexCovered(index, 7) + 1) + costs[1];  // 7 day pass taken 
+        int cost3 = dfs(getLastIndexCovered(index, 30)+ 1) + costs[2];  // 30 day pass taken 
+        
+        minCost[index] = Math.min(cost1, Math.min(cost2, cost3));
+        System.out.println(index +" " +minCost[index]);
+        return minCost[index];
+    }
+    
+    private int getLastIndexCovered(int index, int daysAllowed) {
+        int left = index;
+        int right = days.length-1;
+        int lastIndexCovered = 0;
+        int lastDayAllowed = days[index] + daysAllowed - 1;
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            if (days[mid] <= lastDayAllowed) {
+                lastIndexCovered = mid;
+                left = mid + 1;
+            }
+            else right = mid - 1;
+        }
+        System.out.println(index+ " d "+lastIndexCovered);
+        return lastIndexCovered;
+    }
+}
