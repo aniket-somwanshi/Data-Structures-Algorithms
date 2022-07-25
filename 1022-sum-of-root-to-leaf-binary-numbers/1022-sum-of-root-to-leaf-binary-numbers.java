20e24b5dee48fca71d03c407c9c7440bb924b1c3
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-07-25 13:58:31 +0530

    Time: 1 ms (51.63%), Space: 42.5 MB (53.51%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -1,58 +1,70 @@
 class Solution {
+    // O(N + ~N) O(1) -- Morris preorder traversal for constant space
     public int sumRootToLeaf(TreeNode root) {
-        int rootToLeaf = 0, currNumber = 0;
-        int steps;
-        TreeNode predecessor;
-
-        while (root != null) {
-            // If there is a left child,
-            // then compute the predecessor.
-            // If there is no link predecessor.right = root --> set it.
-            // If there is a link predecessor.right = root --> break it.
-            if (root.left != null) {
-                // Predecessor node is one step to the left
-                // and then to the right till you can.
-                predecessor = root.left;
-                steps = 1;
-                while (predecessor.right != null && predecessor.right != root) {
-                    predecessor = predecessor.right;
-                    ++steps;
+        TreeNode current = root;
+        int res = 0;
+        int currentNumber = 0;
+        while (current != null) {
+            if (current.left == null) {
+                // print current 
+                currentNumber = currentNumber * 2 + current.val;
+                if (current.right == null) {
+                    // this is lead node, update res
+                    res += currentNumber;
                 }
-
-                // Set link predecessor.right = root
-                // and go to explore the left subtree
-                if (predecessor.right == null) {
-                    currNumber = (currNumber << 1) | root.val;
-                    predecessor.right = root;
-                    root = root.left;
-                }
-                // Break the link predecessor.right = root
-                // Once the link is broken,
-                // it's time to change subtree and go to the right
-                else {
-                    // If you're on the leaf, update the sum
-                    if (predecessor.left == null) {
-                        rootToLeaf += currNumber;
-                    }
-                    // This part of tree is explored, backtrack
-                    for(int i = 0; i < steps; ++i) {
-                        currNumber >>= 1;
-                    }
-                    predecessor.right = null;
-                    root = root.right;
-                }
+                current = current.right;
             }
-            // If there is no left child
-            // then just go right.
             else {
-                currNumber = (currNumber << 1) | root.val;
-                // if you're on the leaf, update the sum
-                if (root.right == null) {
-                    rootToLeaf += currNumber;
+                TreeNode lastNode = current.left;
+                int depth = 1;
+                while (lastNode.right != null && lastNode.right != current) {
+                    lastNode = lastNode.right;
+                    depth++;
                 }
-                root = root.right;
+                if (lastNode.right == null) {
+                        // print current
+                        currentNumber = currentNumber * 2 + current.val;
+
+                        lastNode.right = current;
+                        current = current.left;
+                    }
+                else {
+                        // check if it's a leaf node, if so then add currentNumber to res
+                        if (lastNode.left == null) {
+                            res += currentNumber;
+                        }
+
+                        // backtrack currentNumber back to the root, as per the depth
+                        for (int i = 0; i < depth; i++) {
+                            currentNumber /= 2;
+                        }
+                        lastNode.right = null;
+                        current = current.right;
+                    }
+                }
             }
-        }
-        return rootToLeaf;
+        return res;
     }
+    
+    
+    // O(N) O(N)
+//     int res = 0;
+//     public int sumRootToLeaf(TreeNode root) {
+//         dfs(root, 0);
+//         return res;
+//     }
+    
+//     private void dfs(TreeNode node, int currentSum) {
+//         currentSum = (currentSum * 2) + node.val;
+        
+//         if (node.left == null && node.right == null) {
+//             res += currentSum;
+//             return;
+//         }
+        
+//         if (node.left != null) dfs(node.left, currentSum);
+        
+//         if (node.right != null) dfs(node.right, currentSum);
+        
+//     }
 }
