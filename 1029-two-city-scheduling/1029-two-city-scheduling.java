eae1551930afe72aea04bacb389a0d29c85989ca
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-07-27 12:44:58 +0530

    Time: 3 ms (54.07%), Space: 43 MB (8.80%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,22 +1,31 @@
 class Solution {
+    private Integer[][] dp;
+    private int[][] costs;
+    private int n;
     public int twoCitySchedCost(int[][] costs) {
-        if (costs == null || costs.length == 0) return 0;
-        for (int i=0; i<costs.length; i++) {
-            costs[i][0] = costs[i][0] - costs[i][1];
-        }
-        Arrays.sort(costs, (a, b) -> a[0]-b[0]);
+        this.costs = costs;
+        this.n = costs.length;
+        this.dp = new Integer[n][n];
+        return dfs(0, 0, 0, 0);
+    }
+    
+    private int dfs(int index, int peopleA, int peopleB, int cost) {
+        if (index >= n) return 0;
         
-        int sum = 0;
-        // 4/2 = 2 0 1 < 2
-        for (int i=0; i<costs.length; i++) {
-            if (i<costs.length/2) {
-                // go to a
-                sum += costs[i][0] + costs[i][1];
-            } else {
-                // go to b
-                sum += costs[i][1];
-            }
+        if (dp[peopleA][peopleB] != null) {
+            return dp[peopleA][peopleB];
         }
-        return sum;
+        
+        if (peopleA < n/2 && peopleB < n/2) 
+            dp[peopleA][peopleB] =  Math.min(costs[index][0] + dfs(index+1, peopleA + 1, peopleB, cost + costs[index][0]), 
+                       costs[index][1] + dfs(index+1, peopleA, peopleB + 1, cost + costs[index][1]));
+        
+        else if (peopleA < n/2 && peopleB == n/2)
+            dp[peopleA][peopleB] = costs[index][0] + dfs(index+1, peopleA + 1, peopleB, cost + costs[index][0]);
+        
+        else 
+            dp[peopleA][peopleB] = costs[index][1] + dfs(index+1, peopleA, peopleB + 1, cost + costs[index][1]);
+        
+        return dp[peopleA][peopleB];
     }
 }
