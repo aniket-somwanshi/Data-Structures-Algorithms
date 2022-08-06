a45e26e754f5e6104c40d7f048f14a5b4728732c
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-08-06 21:05:48 +0530

    Time: 74 ms (44.12%), Space: 66.3 MB (47.73%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,42 +1,65 @@
 class Solution {
-    Integer[] dp;
-    String[] words;
-    int maxi = 1;
+    // optimized -- Map to lookup a string 
     public int longestStrChain(String[] words) {
-        int n = words.length;
-        this.dp = new Integer[n];
-        this.words = words;
+        Map<String, Integer> map = new HashMap<>();
         Arrays.sort(words, (a, b) -> a.length() - b.length());
-        for (int i = 0; i < n; i++) maxi = Math.max(maxi, chainsFrom(i));
-        return maxi;
-    }
-    
-    private int chainsFrom(int index) {
-        if (index == words.length) return 0;
-        if (dp[index] != null) return dp[index];
-        
-        int ans = 1;
-        for (int j = index+1; j < words.length; j++){
-            if (areChained(words[index], words[j]) == true) {
-                ans = Math.max(ans, 1 + chainsFrom(j));
+        int res = 0;
+        for (String word: words) {
+            map.put(word, 1);
+            for (int i = 0; i < word.length(); i++) {
+                StringBuilder current = new StringBuilder(word);
+                String previous = current.deleteCharAt(i).toString();
+                if (map.containsKey(previous)) {
+                    map.put(word, Math.max(map.get(word), map.get(previous) + 1));
+                }
             }
+            res = Math.max(res, map.get(word));
         }
-        
-        return dp[index] = ans;
-    }
-    
-    private boolean areChained(String smaller, String bigger) {
-        if (smaller.length() != bigger.length()-1) return false;
-        int i = 0;
-        int j = 0;
-        while (j < bigger.length()) {
-            if (i < smaller.length() && smaller.charAt(i) == bigger.charAt(j)) {
-                i++; j++;
-            }
-            else {
-                j++;
-            }
-        }
-        return i == smaller.length() && j == bigger.length();
+        return res;
     }
 }
+// class Solution {
+//     // O(NlogN + N^2)
+//     Integer[] dp;
+//     String[] words;
+//     int maxi = 1;
+//     public int longestStrChain(String[] words) {
+//         int n = words.length;
+//         this.dp = new Integer[n];
+//         this.words = words;
+//         Arrays.sort(words, (a, b) -> a.length() - b.length());
+//         for (int i = 0; i < n; i++) maxi = Math.max(maxi, chainsFrom(i));
+//         return maxi;
+//     }
+    
+//     private int chainsFrom(int index) {
+//         if (index == words.length) return 0;
+//         if (dp[index] != null) return dp[index];
+        
+//         int ans = 1;
+//         for (int j = index+1; j < words.length; j++){
+//             if (areChained(words[index], words[j]) == true) {
+//                 ans = Math.max(ans, 1 + chainsFrom(j));
+//             }
+//         }
+        
+//         return dp[index] = ans;
+//     }
+    
+    
+//     // O(L) ~ O(16)
+//     private boolean areChained(String smaller, String bigger) {
+//         if (smaller.length() != bigger.length()-1) return false;
+//         int i = 0;
+//         int j = 0;
+//         while (j < bigger.length()) {
+//             if (i < smaller.length() && smaller.charAt(i) == bigger.charAt(j)) {
+//                 i++; j++;
+//             }
+//             else {
+//                 j++;
+//             }
+//         }
+//         return i == smaller.length() && j == bigger.length();
+//     }
+// }
