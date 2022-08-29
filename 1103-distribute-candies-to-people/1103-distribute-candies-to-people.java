41e856da8ab088fc0a8d73c5c0c0eb5259a5d36f
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-08-29 14:39:24 +0530

    Time: 19 ms (12.23%), Space: 41 MB (85.87%) - LeetHub

---
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+        int N = getN(candies);
+        
+        int[] res = new int[num_people];
+        int noOfCompleteRows = N/num_people; 
+        
+        for (int i = 0; i < num_people; i++) {
+            int sum = getSumOfNTermsInAP(i+1, noOfCompleteRows, num_people);
+            //System.out.println(sum + " d");
+            res[i] += sum;
+            candies -= sum;
+        }
+        
+        //System.out.println(candies + " "+ noOfCompleteRows+ " "+N);
+        
+        // possibly remaining row
+        int lastRowStart = (noOfCompleteRows * num_people) + 1;
+                //System.out.println("lastRowStart"+lastRowStart);
+
+        int i = 0;
+        while (candies > 0) {
+            if (i == num_people) i = 0;
+            if (candies < lastRowStart) {
+                res[i] += candies;
+                candies -= candies;
+                break;
+            }
+            else {
+                res[i] += lastRowStart;
+                candies -= lastRowStart;
+                lastRowStart++;
+            }
+            i++;
+        }
+        return res;
+    } 
+    
+    private int getSumOfNTermsInAP(int a, int n, int d) {
+        System.out.println(a+" "+n+" "+d+" d");
+        int ans = (n*(2*a + (n-1)*d))/2;
+        System.out.println(ans);
+        return ans;
+
+    }
+    
+    private int getN(int candies) {
+        int k = candies;
+        int N = -1 + (int)(Math.sqrt(1 + 8*k))/2;
+        return N;
+    }
+}
