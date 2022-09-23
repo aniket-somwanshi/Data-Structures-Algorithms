76e4fbb0ab4bc519758bd5a9b85742f3da6d5445
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-09-23 15:35:38 +0530

    Time: 7 ms (21.90%), Space: 54.8 MB (52.47%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int zeros = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) zeros++;
+        }
+        int currectPosition = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[currectPosition] = nums[i];
+                currectPosition++;
+            }    
+        }
+        System.out.println(zeros);
+        int index = n-1;
+        while (zeros > 0) {
+            nums[index--] = 0;
+            zeros--;
+        }
+    }
+}
