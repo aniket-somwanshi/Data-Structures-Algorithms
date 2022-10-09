ba035341858bb787ccfb626a71ea7c8f595c31c4
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-10-09 13:53:13 +0530

    Time: 10 ms (47.22%), Space: 48.6 MB (41.40%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -1,39 +1,51 @@
 class MinStack {
-   
+    Stack<Integer> s;
+    Stack<Integer> ms;
     public MinStack() {
-       
+        s = new Stack<Integer>();
+        ms = new Stack<Integer>();
     }
-    private Stack<Integer> mStack = new Stack<Integer>();
-private Stack<Integer> mMinStack = new Stack<Integer>();
+//     public void push(int x) {
+//         mStack.push(x);
+//         if (mMinStack.size() != 0) {
+//             int min = mMinStack.peek();
+//             if (x <= min) {
+//                 mMinStack.push(x);
+//             }
+//         } else {
+//             mMinStack.push(x);
+//         }
+// }
 
-public void push(int x) {
-	mStack.push(x);
-	if (mMinStack.size() != 0) {
-		int min = mMinStack.peek();
-		if (x <= min) {
-			mMinStack.push(x);
-		}
-	} else {
-		mMinStack.push(x);
-	}
-}
-
-public void pop() {
-	int x = mStack.pop();
-	if (mMinStack.size() != 0) {
-		if (x == mMinStack.peek()) {
-			mMinStack.pop();
-		}
-	}
-}
-
-public int top() {
-	return mStack.peek();
-}
-
-public int getMin() {
-    return mMinStack.peek();
-}
+//     public void pop() {
+//         int x = mStack.pop();
+//         if (mMinStack.size() != 0) {
+//             if (x == mMinStack.peek()) {
+//                 mMinStack.pop();
+//             }
+//         }
+//     }
+    
+    public void push(int val) {
+        s.push(val);
+        if (ms.isEmpty() || val <= ms.peek()) ms.push(val);
+    }
+    
+    public void pop() {
+        int x = s.pop();
+        if (ms.size() != 0) {
+            if (x == ms.peek()) ms.pop();
+        }
+        
+    }
+    
+    public int top() {
+        return s.peek();
+    }
+    
+    public int getMin() {
+        return ms.peek();
+    }
 }
 
 /**
