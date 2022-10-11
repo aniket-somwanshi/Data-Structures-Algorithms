112c2bd7bcb37e58f135ead546db1f820860be62
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-10-11 20:10:33 +0530

    Time: 116 ms (39.90%), Space: 102.7 MB (55.62%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int totalFruit(int[] a) {
+        int n = a.length;
+        Map<Integer, Integer> map = new HashMap<>(); // (fruitType, count)
+        int start = 0;
+        int end = 0;
+        int maxi = 0;
+        while (end < n) {
+            map.put(a[end], map.getOrDefault(a[end], 0) + 1); // increase count of this type
+            
+            // if we have more than 2 fruitTypes, then
+            // we are invalid so try to remove a type from left
+            while (map.size() > 2) {
+                // remove a[start] from map
+                int count = map.get(a[start]);
+                count--;
+                if (count == 0) map.remove(a[start]);
+                else map.put(a[start], count);
+                start++;
+            }
+            
+            
+            maxi = Math.max(maxi, end - start + 1);
+            end++;
+        }
+        return maxi;
+    }
+}
