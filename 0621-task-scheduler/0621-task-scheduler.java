84714106263bcd27e7440fd5f69c9b10998da984
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-10-15 23:16:19 +0530

    Time: 78 ms (21.81%), Space: 59.8 MB (56.47%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int leastInterval(char[] tasks, int k) {
+        int n = tasks.length;
+        int[] count = new int[26];
+        for (char task: tasks) {
+            count[task - 'A']++;
+        }
+        
+        int tasksProcessed = 0;
+        
+        int[] lastUsedAt = new int[26];
+        Arrays.fill(lastUsedAt, Integer.MIN_VALUE);
+        
+        int cycle = 0;
+        while (tasksProcessed < n) {
+            // try to take the most frequent task, which is also valid  
+            int maxiIndex = -1;
+            int maxi = 0;
+            for (int i = 0; i < 26; i++) {
+                if (count[i] > maxi && cycle > lastUsedAt[i] + k) {
+                    maxi = count[i];
+                    maxiIndex = i;
+                }
+            }
+            //System.out.println(maxiIndex);
+            // use this maxi task 
+            if (maxiIndex != -1) {
+                lastUsedAt[maxiIndex] = cycle;
+                count[maxiIndex]--;
+                tasksProcessed++;
+            }
+            cycle++;
+        }
+        return cycle;
+    }
+}
