eb612fc2474eb6da0b1a8ad45a80cc56c5931a02
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-10-16 20:16:25 +0530

    Time: 58 ms (71.75%), Space: 54 MB (75.76%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        int n = hand.length;
+        if (n % groupSize != 0) return false;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int card: hand) minHeap.add(card);
+        
+        while (!minHeap.isEmpty()) {
+            int current = minHeap.poll();
+            List<Integer> unwanted = new LinkedList<>();
+            
+            int elementsTaken = 0;
+            while (!minHeap.isEmpty() && elementsTaken < groupSize - 1) {
+                if (minHeap.peek() == current + 1) {
+                    elementsTaken++;
+                    current = minHeap.poll();
+                }
+                else if (minHeap.peek() == current) {
+                    unwanted.add(minHeap.poll());
+                }
+                else {
+                    return false;
+                }
+            }
+            if (elementsTaken < groupSize - 1) return false;
+            // put the unwanted list back into minHeap
+            // they might be useful later
+            for (int u: unwanted) minHeap.add(u);
+        }
+        return true;
+    }
+}
