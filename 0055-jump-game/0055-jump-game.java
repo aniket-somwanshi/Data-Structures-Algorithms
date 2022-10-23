cc0004fc4bb5c605afb762bc26cfac2cf4c89262
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-10-23 12:48:27 +0530

    Time: 10 ms (35.54%), Space: 68.9 MB (12.93%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxi = 0;
+        int mini = 0;
+        while (mini <= maxi && maxi < nums.length-1) {
+            maxi = Math.max(maxi, mini + nums[mini]);
+            mini++;
+        }
+        
+        return maxi >= nums.length-1;
+    }
+}
