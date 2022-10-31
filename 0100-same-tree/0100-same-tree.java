b370b2f991be5001baa94c3700f65ab67121aedb
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-10-31 19:25:29 +0530

    Time: 1 ms (8.68%), Space: 41.9 MB (16.12%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,11 +1,62 @@
-
+// O(N) O(H) --iterative
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q != null) return false;
         if (q == null && p != null) return false;
         if (p == null) return true;
-        if (p.val != q.val) return false;
-        return isSameTree(p.left, q.left) && 
-            isSameTree(p.right, q.right);   
+        
+        Queue<TreeNode> a = new LinkedList<>();
+        Queue<TreeNode> b = new LinkedList<>();
+        a.add(p);
+        b.add(q);
+        
+        while (!a.isEmpty() && !b.isEmpty()) {
+            int size1 = a.size();
+            int size2 = b.size();
+            if (size1 != size2) return false;
+            
+            for (int i = 0; i < size1; i++) {
+                TreeNode c1 = a.poll();
+                TreeNode c2 = b.poll();
+                
+                if (c1.val != c2.val) return false;
+                
+                if (c1.left == null && c2.left == null) {
+                    
+                }
+                else if (c1.left != null && c2.left != null) {
+                    a.add(c1.left);
+                    b.add(c2.left);
+                }
+                else {
+                    return false;
+                }
+                
+                if (c1.right == null && c2.right == null) {
+                    
+                }
+                else if (c1.right != null && c2.right != null) {
+                    a.add(c1.right);
+                    b.add(c2.right);
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        
+        return a.isEmpty() && b.isEmpty();
     }
 }
+
+// // O(N) O(H) --recursive
+// class Solution {
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
+//         if (p == null && q != null) return false;
+//         if (q == null && p != null) return false;
+//         if (p == null) return true;
+//         if (p.val != q.val) return false;
+//         return isSameTree(p.left, q.left) && 
+//             isSameTree(p.right, q.right);   
+//     }
+// }
