16cfd4d54c958fc263c09a144e1592bcfc38e108
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-11-04 16:53:26 +0530

    Time: 0 ms (100.00%), Space: 49.1 MB (81.15%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -1,49 +1,76 @@
 class Solution {
-    int missing = 0;
-    boolean foundLastLevel = false;
-    int maxDepth = 0;
     public int countNodes(TreeNode root) {
         if (root == null) return 0;
-        maxDepth = getMaxDepth(root);
-        System.out.println(maxDepth);
-        maxDepth++;
-        getMissingNodes(root, 0);
-        System.out.println(missing);
-        int total = (int)Math.pow(2, maxDepth+1) - 1;
-        return total - missing;
+        
+        return getNumberOfNodes(root);
     }
     
-    private void getMissingNodes(TreeNode node, int depth) {
-        if (foundLastLevel) return;
+    private int getNumberOfNodes(TreeNode node) {
+        if (node == null) return 0;
         
-        if (depth == maxDepth) {
-            foundLastLevel = true;
-            return;
-        }
-        
-        if (node == null) return;
-        
-        if (node.right == null) {
-            System.out.println(node.val);
-            missing++;
+        if (getLeftHeight(node) == getRightHeight(node)) {
+            return (int)Math.pow(2, getLeftHeight(node)) - 1;
         }
         else {
-            getMissingNodes(node.right, depth + 1);
+            return 1 + getNumberOfNodes(node.left) + getNumberOfNodes(node.right);
         }
-        
-        if (node.left == null) {
-            missing++;
-            System.out.println(node.val);
-        }
-        else {
-            getMissingNodes(node.left, depth + 1);
-        }
-        
     }
     
-    private int getMaxDepth(TreeNode node) {
-        if (node.right == null) return 0;
-        return 1 + getMaxDepth(node.right);
+    private int getLeftHeight(TreeNode node) {
+        if (node == null) return 0;
+        return 1 + getLeftHeight(node.left);
     }
     
+    private int getRightHeight(TreeNode node) {
+        if (node == null) return 0;
+        return 1 + getRightHeight(node.right);
+    }
+
 }
+
+// // O(N) O(H) 
+// class Solution {
+//     int missing = 0;
+//     boolean foundLastLevel = false;
+//     int maxDepth = 0;
+//     public int countNodes(TreeNode root) {
+//         if (root == null) return 0;
+//         maxDepth = getMaxDepth(root);
+//         maxDepth++;
+//         getMissingNodes(root, 0);
+//         int total = (int)Math.pow(2, maxDepth+1) - 1;
+//         return total - missing;
+//     }
+    
+//     private void getMissingNodes(TreeNode node, int depth) {
+//         if (foundLastLevel) return;
+        
+//         if (depth == maxDepth) {
+//             foundLastLevel = true;
+//             return;
+//         }
+        
+//         if (node == null) return;
+        
+//         if (node.right == null) {
+//             missing++;
+//         }
+//         else {
+//             getMissingNodes(node.right, depth + 1);
+//         }
+        
+//         if (node.left == null) {
+//             missing++;
+//         }
+//         else {
+//             getMissingNodes(node.left, depth + 1);
+//         }
+        
+//     }
+    
+//     private int getMaxDepth(TreeNode node) {
+//         if (node.right == null) return 0;
+//         return 1 + getMaxDepth(node.right);
+//     }
+    
+// }
