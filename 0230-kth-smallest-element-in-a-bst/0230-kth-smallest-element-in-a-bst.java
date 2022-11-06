fdc15a01239372553e11631129a08095c70edab2
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-11-06 14:44:50 +0530

    Time: 2 ms (40.11%), Space: 44.6 MB (80.23%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -1,16 +1,41 @@
+// O(N) O(H) 
 class Solution {
-    int index = 0;
     public int kthSmallest(TreeNode node, int k) {
         if (node == null) return -1;
         
-        int left = kthSmallest(node.left, k);
-        if (left != -1) return left;
+        int index = 0;
+        Stack<TreeNode> s = new Stack<>();
         
-        if (++index == k) return node.val;
-        
-        int right = kthSmallest(node.right, k);
-        if (right != -1) return right;
+        while (node != null || !s.isEmpty()) {
+            while (node != null) {
+                s.push(node);
+                node = node.left;
+            }
+            
+            node = s.pop();
+            if (++index == k) return node.val;
+            
+            node = node.right;
+        }
         
         return -1;
     }
 }
+
+// // O(N) O(H) 
+// class Solution {
+//     int index = 0;
+//     public int kthSmallest(TreeNode node, int k) {
+//         if (node == null) return -1;
+        
+//         int left = kthSmallest(node.left, k);
+//         if (left != -1) return left;
+        
+//         if (++index == k) return node.val;
+        
+//         int right = kthSmallest(node.right, k);
+//         if (right != -1) return right;
+        
+//         return -1;
+//     }
+// }
