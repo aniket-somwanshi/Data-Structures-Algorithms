49a1092bb7f8293bdb62431456486aa9d256a9f5
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-11-06 12:11:36 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (96.24%) - LeetHub

---
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -0,0 +1,55 @@
+// O(H) O(H) --iterative
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        
+        TreeNode node = root;
+        while (node != null) {
+            if (node.val > val) {
+                // go left
+                if (node.left == null) {
+                    node.left = new TreeNode(val);
+                    return root;
+                }
+                node = node.left;
+            }
+            else {
+                if (node.right == null) {
+                    node.right = new TreeNode(val);
+                    return root;
+                }
+                node = node.right;
+            }
+        }
+        return root;
+    }
+}
+
+// O(H) O(H) --recursive
+// class Solution {
+//     public TreeNode insertIntoBST(TreeNode root, int val) {
+//         if (root == null) return new TreeNode(val);
+//         dfs(root, val);
+//     		return root;
+// }
+// 	private void dfs(TreeNode node, int val) {
+// 	if (node == null) return;
+// 	if (node.val > val) {
+// 	// go left
+// 	if (node.left == null) {
+// 	node.left = new TreeNode(val);
+// 	return;
+// }
+// dfs(node.left, val);
+// }
+// else {
+// 	if (node.right == null) {
+// 	node.right = new TreeNode(val);
+// 	return;
+// }
+// dfs(node.right, val);
+// }
+// }
+// }
+
+
