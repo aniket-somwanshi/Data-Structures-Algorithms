3491b5841deb3bc3a9b5a9fa54004ad76212f492
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-11-07 11:15:08 +0530

    Time: 6 ms (7.63%), Space: 44.6 MB (7.94%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,21 +1,57 @@
+// O(N) O(H) -- iterative 
+// inorder -- in an inorder travsersal, elements should be in increasing order, so if a node is not increasing, return false
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
-    
-    private boolean validate(TreeNode node, long mini, long maxi) {
-        if (node == null) return true;
+        if (root == null) return true;
         
-        if (node.val <= mini) {
-            System.out.println(node.val + "<=" + mini);
-            return false;
-        }
-        if (node.val >= maxi) {
-            System.out.println(node.val + ">=" + maxi);
-            return false;
-        }
+        Stack<TreeNode> s = new Stack<>();
+        TreeNode node = root;
+        TreeNode previous = null;
         
-        return validate(node.left, mini, node.val)
-            && validate(node.right, node.val, maxi);
+        while (!s.isEmpty() || node != null) {
+            // push lefts first
+            while (node != null) {
+                s.push(node);
+                node = node.left;
+            }
+            
+            // do 
+            node = s.pop();
+            if (previous != null && node.val <= previous.val) return false;
+            
+            // go to right
+            previous = node;
+            node = node.right;
+        }
+        return true;
     }
 }
+
+
+
+
+
+
+
+// O(N) O(H) -- recursive
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+//     }
+    
+//     private boolean validate(TreeNode node, long mini, long maxi) {
+//         if (node == null) return true;
+        
+//         if (node.val <= mini) {
+//             System.out.println(node.val + "<=" + mini);
+//             return false;
+//         }
+//         if (node.val >= maxi) {
+//             System.out.println(node.val + ">=" + maxi);
+//             return false;
+//         }
+        
+//         return validate(node.left, mini, node.val)
+//             && validate(node.right, node.val, maxi);
+//     }
+// }
