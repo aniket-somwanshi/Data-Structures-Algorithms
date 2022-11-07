79c4412fefd0f1698763c60249b5ccbb26005b67
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-11-07 11:32:24 +0530

    Time: 23 ms (7.70%), Space: 50.5 MB (6.84%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,19 +1,44 @@
-
+// // O(H) O(1) -- iterative
+// // if a node sits in between [p, q] then it's the LCA
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (p.val > q.val) return lca(root, q, p);
-        return lca(root, p, q);
+        if (p.val > q.val) return lowestCommonAncestor(root, q, p);
+        
+        if (root == null) return null;
+        TreeNode node = root;
+        
+        while (node != null) {
+            if (p.val <= node.val && node.val <= q.val) return node;
+            
+            if (node.val < p.val) node = node.right;
+        
+            else node = node.left;
+        }
+        
+        return null;
     }
+}    
+
+
+
+
+
+
+// // O(H) O(H) -- recursive
+// // if a node sits in between [p, q] then it's the LCA
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if (p.val > q.val) return lca(root, q, p);
+//         return lca(root, p, q);
+//     }
     
-    private TreeNode lca(TreeNode node, TreeNode p, TreeNode q) {
-        if (node == null) return null;
+//     private TreeNode lca(TreeNode node, TreeNode p, TreeNode q) {
+//         if (node == null) return null;
         
-        if (p.val <= node.val && node.val <= q.val) return node;
+//         if (p.val <= node.val && node.val <= q.val) return node;
         
-        if (node.val < p.val) return lca(node.right, p, q);
+//         if (node.val < p.val) return lca(node.right, p, q);
         
-        else return lca(node.left, p, q);
-    }
-    
-    
-}
+//         else return lca(node.left, p, q);
+//     } 
+// }
