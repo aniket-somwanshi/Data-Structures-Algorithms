82b184c273d2901cab0a4f9632c7e9a331ffeb4d
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-11-09 22:28:12 +0530

    Time: 1 ms (99.95%), Space: 42.9 MB (94.10%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,32 +1,96 @@
+// O(V+E) O(V+E) -- DSU union find
+class DSU {
+    int[] size;
+    int[] rank;
+    int[] parent;
+    int count;
+    public DSU(int n) {
+        this.size = new int[n];
+        this.rank = new int[n];
+        this.parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        count = n;
+    }
+    
+    public boolean isSameComponent(int u, int v) {
+        return findParent(u) == findParent(v);
+    }
+    
+    private int findParent(int node) {
+        if (parent[node] == node) return node;
+        else return parent[node] = findParent(parent[node]);
+    } 
+    
+    public void unionBySize(int u, int v) {
+        int p1 = findParent(u);
+        int p2 = findParent(v);
+        
+        if (p1 == p2) return;
+        
+        if (size[p1] < size[p2]) {
+            // attach p1 to p2
+            parent[p1] = p2;
+            size[p2]++;
+        }
+        else {
+            parent[p2] = p1;
+            size[p1]++;
+        }
+        count--;
+    }
+}
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
         
-        for (int from = 0; from < n; from++) {
-            for (int to = 0; to < n; to++) {
-                if (isConnected[to][from] == 1 && from != to) {
-                    adj.get(from).add(to);
-                }
-            }
-        }
+        DSU dsu = new DSU(n);
         
-        int c = 0;
-        boolean[] visited = new boolean[n];
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                c++;
-                dfs(i, adj, visited);
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    dsu.unionBySize(i, j);
+                } 
             }
         }
-        return c;
-    }
-    
-    private void dfs(int node, List<List<Integer>> adj, boolean[] visited) {
-        visited[node] = true;
-        for (int neighbor: adj.get(node)) {
-            if (!visited[neighbor]) dfs(neighbor, adj, visited);
-        }
+        
+        return dsu.count;
     }
 }
+
+
+
+// O(V+E) O(V+E) -- dfs
+// class Solution {
+//     public int findCircleNum(int[][] isConnected) {
+//         int n = isConnected.length;
+//         List<List<Integer>> adj = new ArrayList<>();
+//         for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        
+//         for (int from = 0; from < n; from++) {
+//             for (int to = 0; to < n; to++) {
+//                 if (isConnected[to][from] == 1 && from != to) {
+//                     adj.get(from).add(to);
+//                 }
+//             }
+//         }
+        
+//         int c = 0;
+//         boolean[] visited = new boolean[n];
+//         for (int i = 0; i < n; i++) {
+//             if (!visited[i]) {
+//                 c++;
+//                 dfs(i, adj, visited);
+//             }
+//         }
+//         return c;
+//     }
+    
+//     private void dfs(int node, List<List<Integer>> adj, boolean[] visited) {
+//         visited[node] = true;
+//         for (int neighbor: adj.get(node)) {
+//             if (!visited[neighbor]) dfs(neighbor, adj, visited);
+//         }
+//     }
+// }
