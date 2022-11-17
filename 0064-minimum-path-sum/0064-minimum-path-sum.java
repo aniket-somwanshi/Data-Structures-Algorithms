fa3f78b18e64f4d987a7d790ac4780c410efd87e
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-11-17 11:40:39 +0530

    Time: 2 ms (93.71%), Space: 45 MB (86.65%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minPathSum(int[][] a) {
+        int rows = a.length, cols = a[0].length;
+        int[] up = new int[cols];
+        up[0] = a[0][0];
+        for (int j = 1; j < cols; j++) {
+            up[j] = up[j-1] + a[0][j];
+        }
+        
+        for (int i = 1; i < rows; i++) {
+            int[] current = new int[cols];
+            for (int j = 0; j < cols; j++) {
+                if (j == 0) current[j] = up[j] + a[i][j];
+                else current[j] = a[i][j] + Math.min(current[j-1], up[j]);
+            }
+            up = current;
+        }
+        return up[cols-1];
+    }
+}
