6c274dcc4c27c61e4254a15cb9f155d31152f8b4
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-11-18 13:18:24 +0530

    Time: 6 ms (79.83%), Space: 39.8 MB (96.19%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,22 +1,49 @@
+// O(N*AMOUNT) O(2*AMOUNT) --dp space opt
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
+        int[] up = new int[amount+1];
+        int[] current = new int[amount+1];
         
         for (int i = 1; i < n+1; i++) {
             for (int j = 0; j < amount+1; j++) {
                 if (j == 0) {
-                    dp[i][j] = 1;
+                    current[j] = 1;
                 }
                 else if (coins[i-1] <= j){
-                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                    current[j] = up[j] + current[j-coins[i-1]];
                 }
                 else {
-                    dp[i][j] = dp[i-1][j];
+                    current[j] = up[j];
                 }
             }
+            up = current;
         }
         
-        return dp[n][amount];
+        return up[amount];
     }
 }
+
+// // O(N*AMOUNT) O(N*AMOUNT) --dp
+// class Solution {
+//     public int change(int amount, int[] coins) {
+//         int n = coins.length;
+//         int[][] dp = new int[n+1][amount+1];
+        
+//         for (int i = 1; i < n+1; i++) {
+//             for (int j = 0; j < amount+1; j++) {
+//                 if (j == 0) {
+//                     dp[i][j] = 1;
+//                 }
+//                 else if (coins[i-1] <= j){
+//                     dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+//                 }
+//                 else {
+//                     dp[i][j] = dp[i-1][j];
+//                 }
+//             }
+//         }
+        
+//         return dp[n][amount];
+//     }
+// }
