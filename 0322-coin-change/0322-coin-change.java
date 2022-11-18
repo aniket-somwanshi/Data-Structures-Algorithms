1759549d85b8a5bbc37193287d63d56157ab8091
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-11-18 11:24:05 +0530

    Time: 21 ms (87.09%), Space: 42.6 MB (85.19%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,20 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
+        //int[][] dp = new int[n+1][amount+1];
+        int[] up = new int[amount+1];
+        int[] current = new int[amount+1];
         
         for (int i = 0; i <= n; i++) {
             for (int j = 1; j <= amount; j++) {
                 if (i == 0) {
-                    dp[i][j] = Integer.MAX_VALUE;
+                    current[j] = Integer.MAX_VALUE;
                 }
-                else if (coins[i-1] <= j && dp[i][j-coins[i-1]] < Integer.MAX_VALUE){
-                   dp[i][j] = Math.min(dp[i-1][j], 1 + dp[i][j-coins[i-1]]);
+                else if (coins[i-1] <= j && current[j-coins[i-1]] < Integer.MAX_VALUE){
+                    current[j] = Math.min(up[j], 1 + current[j-coins[i-1]]);
                 }
-                else dp[i][j] = dp[i-1][j];
+                else current[j] = up[j];
             }
+            up = current;
         }
         
-        return dp[n][amount] == Integer.MAX_VALUE ? -1 : dp[n][amount];
+        return up[amount] == Integer.MAX_VALUE ? -1 : up[amount];
     }
 }
