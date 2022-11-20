8af0b73872d86d946c208db7c9ae5935c637794d
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-11-20 23:45:37 +0530

    Time: 9 ms (87.90%), Space: 44.7 MB (33.52%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,33 +1,63 @@
+// O(NlogN) O(N) --binary search
 class Solution {
-    // O(N*N) O(N*N) --memo
-    // pick the current element if it extends current LIS,
-    // also skip current element
     public int lengthOfLIS(int[] a) {
         int n = a.length;
-        Integer[][] memo = new Integer[n][n];
-        
-        return recur(0, -1, a, memo);
+        int[] res = new int[n];
+        int lastIndex = 0;
+        res[lastIndex++] = a[0];
+        for (int i = 1; i < n; i++) {
+            if (a[i] > res[lastIndex-1]) {
+                res[lastIndex++] = a[i];
+            }
+            else {
+                int position = getPosition(0, lastIndex-1, res, a[i]);
+                res[position] = a[i];
+            }
+        }
+        return lastIndex;
     }
     
-    private int recur(int i, int prevIndex, int[] a, Integer[][] memo) {
-        if (i == a.length) return 0;
-        
-        if (prevIndex != -1 && memo[i][prevIndex] != null) return memo[i][prevIndex];  
-        
-        int take = 0;
-        // take if possible  
-        if (prevIndex == -1 || a[i] > a[prevIndex]) {
-            take = 1 + recur(i+1, i, a, memo);
+    private int getPosition(int start, int end, int[] a, int target) {
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if (a[mid] == target) return mid;
+            else if (target > a[mid]) start = mid + 1;
+            else end = mid - 1;
         }
-        
-        // not take
-        int notTake = recur(i+1, prevIndex, a, memo);
-        
-        int res = Math.max(take, notTake);
-        if (prevIndex != -1) memo[i][prevIndex] = res;
-        return res;
+        return start;
     }
-} 
+}
+
+// class Solution {
+//     // O(N*N) O(N*N) --memo
+//     // pick the current element if it extends current LIS,
+//     // also skip current element
+//     public int lengthOfLIS(int[] a) {
+//         int n = a.length;
+//         Integer[][] memo = new Integer[n][n];
+        
+//         return recur(0, -1, a, memo);
+//     }
+    
+//     private int recur(int i, int prevIndex, int[] a, Integer[][] memo) {
+//         if (i == a.length) return 0;
+        
+//         if (prevIndex != -1 && memo[i][prevIndex] != null) return memo[i][prevIndex];  
+        
+//         int take = 0;
+//         // take if possible  
+//         if (prevIndex == -1 || a[i] > a[prevIndex]) {
+//             take = 1 + recur(i+1, i, a, memo);
+//         }
+        
+//         // not take
+//         int notTake = recur(i+1, prevIndex, a, memo);
+        
+//         int res = Math.max(take, notTake);
+//         if (prevIndex != -1) memo[i][prevIndex] = res;
+//         return res;
+//     }
+//} 
 
 
 // class Solution {
