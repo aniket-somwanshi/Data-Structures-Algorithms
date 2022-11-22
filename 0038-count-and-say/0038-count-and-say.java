4f4b74732ff3851d45aa4ff7cf8d760d0f995038
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-11-22 23:30:46 +0530

    Time: 59 ms (12.41%), Space: 62 MB (12.88%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,7 +1,13 @@
+// O(N*N) O(N) -- dp -- C=unknown constant
 class Solution {
     public String countAndSay(int n) {
-        if (n == 1) return "1";
-        return say(countAndSay(n-1));
+        String[] dp = new String[n+1];
+        dp[1] = "1";
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = say(dp[i-1]);
+        }
+        return dp[n];
     }
     
     private String say(String input) {
@@ -21,3 +27,29 @@ class Solution {
         return res;
     }
 }
+
+
+// // O(N*N) O(N) -- recursion -- C=unknown constant
+// class Solution {
+//     public String countAndSay(int n) {
+//         if (n == 1) return "1";
+//         return say(countAndSay(n-1));
+//     }
+    
+//     private String say(String input) {
+//         String res = "";
+//         int i = 0;
+//         while (i < input.length()) {
+//             // reset counts
+//             char currentNumber = input.charAt(i);
+//             int currentNumberCount = 0;
+//             // find cout of current character
+//             while (i < input.length() && input.charAt(i) == currentNumber) {
+//                 currentNumberCount++;
+//                 i++;
+//             }
+//             res += Integer.toString(currentNumberCount) + currentNumber;
+//         }
+//         return res;
+//     }
+// }
