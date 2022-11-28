de736d44e11921a22795236f98558e0e1e3bae66
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-11-28 13:29:14 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (88.90%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,20 +1,34 @@
 class Solution {
+    // O(logN) O(1)
+    // at the index `mid`, the countOfMissingNumbers = mid - a[mid] 
+    // if it's <= k, we need more missingNumbers, so go right
+    // else go left
+    // at the end the right goes before left
+    // at this point, we know our target number lies betn right to a[right]
+    // and we know at this point, what's the countOfMissingNumbers
+    // we may need some more so as to get to k
+    // so required number is a[right] + (remaining)
+    // = a[right] + (k - countOfMissingNumbers)
     public int findKthPositive(int[] a, int k) {
+        // if the kth element is even behind a[0]
+        // then return kth number directly
+        //if (k < a[0]) return k;
+        
+        int n = a.length;
         int left = 0;
-        int right = a.length-1;
-        int res = -1;
-        if (k < a[0]) return k;
+        int right = n-1;
+        int countOfMissingNumbersAtThisPoint = a[n-1] - n;
         while (left <= right) {
             int mid = left + (right - left)/2;
-            if (a[mid] <= k + mid) {
-                res = k + (mid+1);
-                left = mid + 1;
+            
+             countOfMissingNumbersAtThisPoint = a[mid] - (mid + 1);
+            if (countOfMissingNumbersAtThisPoint >= k) {
+                right = mid-1;
             }
-            else {
-                right = mid - 1;
-            }    
+            else left = mid + 1;
         }
-        return res;
+        if (right == -1) return k;
+        return a[right] + k - (a[right] - (right + 1));
     }
 }
 
