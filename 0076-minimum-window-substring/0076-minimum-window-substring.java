6be5f348825b976158f32f420f631f9794f3f4a5
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-12-08 23:23:11 +0530

    Time: 72 ms (18.10%), Space: 43 MB (84.34%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,66 +1,47 @@
 class Solution {
     public String minWindow(String s, String t) {
-
-        if (s.length() == 0 || t.length() == 0) {
-            return "";
+        int n = s.length();
+        char[] a = s.toCharArray();
+        char[] target = t.toCharArray();
+        
+        // maps
+        int[] freq = new int[256];
+        int[] targetFreq = new int[256];
+        
+        // get target's frequencies
+        for (int i = 0; i < target.length; i++) {
+            targetFreq[target[i]]++;
         }
-
-        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
-
-        for (int i = 0; i < t.length(); i++) {
-            int count = dictT.getOrDefault(t.charAt(i), 0);
-            dictT.put(t.charAt(i), count + 1);
-        }
-
-        int required = dictT.size();
-
-        // Filter all the characters from s into a new list along with their index.
-        // The filtering criteria is that the character should be present in t.
-        List<Pair<Integer, Character>> filteredS = new ArrayList<Pair<Integer, Character>>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (dictT.containsKey(c)) {
-                filteredS.add(new Pair<Integer, Character>(i, c));
-            }
-        }
-
-        int l = 0, r = 0, formed = 0;
-        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
-        int[] ans = { -1, 0, 0 };
-
-        // Look for the characters only in the filtered list instead of entire s.
-        // This helps to reduce our search.
-        // Hence, we follow the sliding window approach on as small list.
-        while (r < filteredS.size()) {
-            char c = filteredS.get(r).getValue();
-            int count = windowCounts.getOrDefault(c, 0);
-            windowCounts.put(c, count + 1);
-
-            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
-                formed++;
-            }
-
-            // Try and contract the window till the point where it ceases to be 'desirable'.
-            while (l <= r && formed == required) {
-                c = filteredS.get(l).getValue();
-
-                // Save the smallest window until now.
-                int end = filteredS.get(r).getKey();
-                int start = filteredS.get(l).getKey();
-                if (ans[0] == -1 || end - start + 1 < ans[0]) {
-                    ans[0] = end - start + 1;
-                    ans[1] = start;
-                    ans[2] = end;
+        
+        
+        int i = 0;
+        int j = 0;
+        
+        int[] mini = new int[] { Integer.MAX_VALUE }; // { mini, start, end }
+        
+        while (j < n) {
+            freq[a[j]]++;
+            // if we have found a valid winodw, 
+            // update the res and try to shrink the window to get a better ans
+            // do this till make the window invalid again
+            while (i <= j && isValid(freq, targetFreq)) {
+                if (j-i+1 < mini[0]) {
+                    mini = new int[] {j-i+1, i, j};
                 }
-
-                windowCounts.put(c, windowCounts.get(c) - 1);
-                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
-                    formed--;
-                }
-                l++;
+                freq[a[i]]--;
+                i++;
             }
-            r++;
+            // at this point the window is invalid
+            j++;
         }
-        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+        
+        return mini[0] == Integer.MAX_VALUE ? "" : s.substring(mini[1], mini[2]+1);
+    }
+    
+    private boolean isValid(int[] freq, int[] targetFreq) {
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] < targetFreq[i]) return false;
+        }
+        return true;
     }
 }
