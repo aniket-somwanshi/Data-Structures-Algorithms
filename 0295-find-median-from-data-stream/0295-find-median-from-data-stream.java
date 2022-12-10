0aebde85dfbd0a4a9e4a25cfefde208dcfabac41
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-12-10 18:56:41 +0530

    Time: 200 ms (69.66%), Space: 126.3 MB (18.96%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,24 +1,51 @@
 class MedianFinder {
-
-    private PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
-private PriorityQueue<Integer> large = new PriorityQueue<>();
-private boolean even = true;
-
-public double findMedian() {
-    if (even)
-        return (small.peek() + large.peek()) / 2.0;
-    else
-        return small.peek();
-}
-
-public void addNum(int num) {
-    if (even) {
-        large.offer(num);
-        small.offer(large.poll());
-    } else {
-        small.offer(num);
-        large.offer(small.poll());
+    PriorityQueue<Integer> leftHalf;
+    PriorityQueue<Integer> rightHalf;
+    
+    public MedianFinder() {
+        leftHalf = new PriorityQueue<Integer>((a,b)->b-a); // max heap
+        rightHalf = new PriorityQueue<Integer>(); // min heap
     }
-    even = !even;
+    
+    public void addNum(int num) {
+        // we should always keep the 2 halves balanced
+        // in terms of number of elements
+        leftHalf.add(num);
+        
+        if (rightHalf.size() > 0 && leftHalf.peek() > rightHalf.peek()) {
+            int wrongInsertion = leftHalf.poll();
+            rightHalf.add(wrongInsertion);
+        }
+        
+        // size imbalance
+        if (leftHalf.size() > rightHalf.size()+1) {
+            int extraElement = leftHalf.poll();
+            rightHalf.add(extraElement);
+        }
+        if (rightHalf.size() > leftHalf.size()+1) {
+            int extraElement = rightHalf.poll();
+            leftHalf.add(extraElement);
+        }
+    }
+    
+    public double findMedian() {
+        // if one of them has an extra number, that is the median
+        if (leftHalf.size() > rightHalf.size()) {
+            return leftHalf.peek();
+        }
+        else if (rightHalf.size() > leftHalf.size()) {
+            return rightHalf.peek();
+        }
+        else {
+            // they are equal so both it's the median of 2 middle numbers
+            return (leftHalf.peek() + rightHalf.peek())/2.0;
+        }
+    }
 }
-}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
