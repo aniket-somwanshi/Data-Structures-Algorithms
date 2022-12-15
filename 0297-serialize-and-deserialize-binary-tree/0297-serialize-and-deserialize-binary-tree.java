86938c075ba946e52c1ed732efbc5126b8233af7
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-12-15 12:17:18 +0530

    Time: 14 ms (88.75%), Space: 43.5 MB (98.38%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,66 @@
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        List<String> encoded = new ArrayList<>();
+        
+        if (root == null) return "";
+        
+        // level order traversal
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        
+        boolean legitNodesAdded = true;
+        while (!q.isEmpty()) {
+            if (!legitNodesAdded) break;
+            legitNodesAdded = false;
+            
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = q.poll();
+                if (current == null) encoded.add("#"); 
+                else {
+                    encoded.add(String.valueOf(current.val));
+                    if (current.left != null || current.right != null) legitNodesAdded = true;
+                    q.add(current.left);
+                    q.add(current.right);
+                }
+            }
+        }
+        return String.join(",", encoded);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == "") return null;
+        
+        String[] encoded = data.split(",");
+        int n = encoded.length;
+        TreeNode[] nodes = new TreeNode[n];
+        // create nodes
+        for (int i = 0; i < n; i++) {
+            if (encoded[i].equals("#")) continue;
+            nodes[i] = new TreeNode(Integer.valueOf(encoded[i]));
+        }
+        
+        // create child links
+        int childPointer = 0;
+        for (int i = 0; i < n; i++) {
+            if (nodes[i] == null) continue;
+            
+            TreeNode current = nodes[i];
+            current.left = childPointer + 1 < n ? nodes[++childPointer] : null;
+            current.right = childPointer + 1 < n ? nodes[++childPointer] : null;
+            
+        }
+        
+        return nodes[0];
+    }
+    
+   
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
