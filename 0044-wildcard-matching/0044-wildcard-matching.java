e85596b6008bcee3daabd106299ddd6967d59c46
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-12-21 23:09:45 +0530

    Time: 23 ms (79.64%), Space: 48.7 MB (40.64%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,4 +1,4 @@
-// O(N*M) O(N*M) -- dp --bottom up 
+// O(N*M) O(2M) -- dp --bottom up  -- space optimization
 class Solution {
     char[] s1;
     char[] s2;
@@ -10,62 +10,88 @@ class Solution {
         s2 = s.toCharArray();
         n = s1.length;
         m = s2.length;
-        boolean[][] dp = new boolean[n+1][m+1];
+        boolean[] next = new boolean[m+1];
+        boolean[] curr = new boolean[m+1];
         
         // base case 
-        for (int i = 0; i < n+1; i++) {
-            int index = i;
-            while (index < n && s1[index] == '*') index++;
-            dp[i][m] = index == n;
-        }
-        dp[n][m] = true; 
+        
+        next[m] = true; 
         
         for (int i = n-1; i >= 0; i--) {
+            curr = new boolean[m+1];
+            // base case
+            int index = i;
+            while (index < n && s1[index] == '*') index++;
+            curr[m] = index == n;
+            
             for (int j = m-1; j >= 0; j--) {
                 boolean res = false;
         
                 if (s1[i] == '*') {
-                    res = res | dp[i][j+1];
-                    res = res | dp[i+1][j]; 
+                    res = res | curr[j+1];
+                    res = res | next[j]; 
                 }
                 else if (s1[i] == '?') {
-                    res = res | dp[i+1][j+1];
+                    res = res | next[j+1];
                 }
                 else {
-                    res = res | s1[i] == s2[j] ? dp[i+1][j+1] : false;
+                    res = res | s1[i] == s2[j] ? next[j+1] : false;
                 }
 
-                dp[i][j] = res;
+                curr[j] = res;
             }
+            next = curr;
         }
     
-        return dp[0][0];
-    }
-    
-    private boolean f(int i, int j) {
-        if (i == n) return j == m;
-        if (j == m) {
-            int index = i;
-            while (i < n && s1[i] == '*') i++;
-            return i == n;
-        }
-        
-        boolean res = false;
-        
-        if (s1[i] == '*') {
-            if (f(i,j+1)) return memo[i][j] = true;
-            if (f(i+1, j)) return memo[i][j] = true;
-        }
-        else if (s1[i] == '?') {
-            res = res | f(i+1, j+1);
-        }
-        else {
-            res = res | s1[i] == s2[j] ? f(i+1, j+1) : false;
-        }
-        
-        return memo[i][j] = res;
+        return next[0];
     }
 }
+
+
+// // O(N*M) O(N*M) -- dp --bottom up 
+// class Solution {
+//     char[] s1;
+//     char[] s2;
+//     int n;
+//     int m;
+//     Boolean[][] memo;
+//     public boolean isMatch(String s, String p) {
+//         s1 = p.toCharArray();
+//         s2 = s.toCharArray();
+//         n = s1.length;
+//         m = s2.length;
+//         boolean[][] dp = new boolean[n+1][m+1];
+        
+//         // base case 
+//         for (int i = 0; i < n+1; i++) {
+//             int index = i;
+//             while (index < n && s1[index] == '*') index++;
+//             dp[i][m] = index == n;
+//         }
+//         dp[n][m] = true; 
+        
+//         for (int i = n-1; i >= 0; i--) {
+//             for (int j = m-1; j >= 0; j--) {
+//                 boolean res = false;
+        
+//                 if (s1[i] == '*') {
+//                     res = res | dp[i][j+1];
+//                     res = res | dp[i+1][j]; 
+//                 }
+//                 else if (s1[i] == '?') {
+//                     res = res | dp[i+1][j+1];
+//                 }
+//                 else {
+//                     res = res | s1[i] == s2[j] ? dp[i+1][j+1] : false;
+//                 }
+
+//                 dp[i][j] = res;
+//             }
+//         }
+    
+//         return dp[0][0];
+//     }
+// }
 
 // // O(N*M) O(N*M + N) -- memo --top down-- TLE !!
 // class Solution {
