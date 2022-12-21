83c052773c6155c394a261d63685401e40861cf1
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-12-21 13:08:13 +0530

    Time: 24 ms (66.10%), Space: 50.8 MB (9.31%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,4 +1,4 @@
-// O(N^2) O(N^2) -- dp tabulation -bottom up
+// O(N^2) O(2N) -- dp space optimization
 class Solution {
     char[] s1;
     char[] s2;
@@ -12,32 +12,81 @@ class Solution {
         
         if (m > n) return 0;
         
-        int[][] dp = new int[n+1][m+1];
+        int[] curr = new int[m+1];
+        int[] next = new int[m+1];
         
         // base 
-        for (int j = 0; j < m+1; j++) dp[n][j] = 0;
-        for (int i = 0; i < n+1; i++) dp[i][m] = 1;
+        //for (int j = 0; j < m+1; j++) dp[n][j] = 0;
+        for (int j = 0; j < m+1; j++) next[j] = 0;
+        //for (int i = 0; i < n+1; i++) dp[i][m] = 1;
+        next[m] = 1;
         
         for (int i = n-1; i >= 0; i--) {
+            curr = new int[m+1];
+            curr[m] = 1;
             for (int j = m-1; j >= 0; j--) {
 
                 int res = 0;
 
                 // matching
-                if (s1[i] == s2[j]) res += dp[i+1][j+1];
-
-                // need to move s1
-                res += dp[i+1][j];
+                //if (s1[i] == s2[j]) res += dp[i+1][j+1];
+                if (s1[i] == s2[j]) res += next[j+1];
                 
-                dp[i][j] = res;
+                // need to move s1
+                // res += dp[i+1][j];
+                res += next[j];
+                
+                // dp[i][j] = res;
+                curr[j] = res;
             }
+            next = curr;
         }
         
-        System.out.println(dp[0][0]);
-        return dp[0][0];
+        // return dp[0][0];
+        return next[0];
     }
     
 }
+
+// // O(N^2) O(N^2) -- dp tabulation -bottom up
+// class Solution {
+//     char[] s1;
+//     char[] s2;
+//     int n;
+//     int m;
+//     public int numDistinct(String s, String t) {
+//         s1 = s.toCharArray();
+//         s2 = t.toCharArray();
+//         n = s1.length;
+//         m = s2.length;
+        
+//         if (m > n) return 0;
+        
+//         int[][] dp = new int[n+1][m+1];
+        
+//         // base 
+//         for (int j = 0; j < m+1; j++) dp[n][j] = 0;
+//         for (int i = 0; i < n+1; i++) dp[i][m] = 1;
+        
+//         for (int i = n-1; i >= 0; i--) {
+//             for (int j = m-1; j >= 0; j--) {
+
+//                 int res = 0;
+
+//                 // matching
+//                 if (s1[i] == s2[j]) res += dp[i+1][j+1];
+
+//                 // need to move s1
+//                 res += dp[i+1][j];
+                
+//                 dp[i][j] = res;
+//             }
+//         }
+        
+//         return dp[0][0];
+//     }
+    
+// }
 
 
 // // O(N^2) O(N^2 + N) -- memo top down
