5242e7f8c559bdf618be0a586f69a2720da6ee70
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2022-12-22 13:37:10 +0530

    Time: 303 ms (14.53%), Space: 96.5 MB (30.36%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,47 @@
+class Solution {
+    int[] a;
+    int n;
+    Integer[][][] memo;
+    public int maxProfit(int[] a) {
+        this.a = a;
+        n = a.length;
+        memo = new Integer[n][3][2];
+        return f(0, 2, 0);
+    }
+    
+    private int f(int i, int k, int needToSell) {
+        // base cases
+        if (i == n) return 0;
+        if (k == 0) return 0;
+        
+        // caching
+        if (memo[i][k][needToSell] != null) return memo[i][k][needToSell];
+        
+        // exploration
+        int res = 0;
+        
+        if (k > 0) {
+            // we have transactions left
+            if (needToSell == 1) {
+                // we need to sell this guy 
+                int candidate1 = a[i] + f(i+1, k-1, 0); // sell this
+                int candidate2 = f(i+1, k, needToSell); // skip this
+                
+                res = Math.max(res, Math.max(candidate1, candidate2));
+            }
+            else {
+                // we need to buy
+                int candidate1 = -a[i] + f(i+1, k, 1); // buy this
+                int candidate2 = f(i+1, k, needToSell); // skip this
+                
+                res = Math.max(res, Math.max(candidate1, candidate2));
+            }
+        }
+        else {
+            // we dont' have transactions left
+            res = 0;
+        }
+        
+        return memo[i][k][needToSell] = res;
+    }
+}
