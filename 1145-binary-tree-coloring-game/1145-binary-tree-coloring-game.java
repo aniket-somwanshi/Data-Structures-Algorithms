9d841968786d377a2c69eab8ec9654840308eae0
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-02-16 15:39:13 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (53.33%) - LeetHub

--- 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
@@ -1,4 +1,4 @@
-// O(3N) O(N)
+// O(2N) O(N) 
 class Solution {
     public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
         // for node x,
@@ -13,21 +13,13 @@ class Solution {
         int leftCount = countNodes(nodeX.left);
         int rightCount = countNodes(nodeX.right);
         
-        int topCount = getTopCount(root, nodeX);
-        
+        int topCount = n - (leftCount + rightCount + 1); 
+            
         int bestScope = Math.max(leftCount, Math.max(rightCount, topCount));
         
         int possibleScope = leftCount + rightCount + topCount + 1 - bestScope;
         
         return possibleScope < (n/2) + 1;
-    }
-    
-    private int getTopCount(TreeNode node, TreeNode nodeX) {
-        if (node == null) return 0;
-        
-        if (node == nodeX) return 0;
-        
-        return getTopCount(node.left, nodeX) + getTopCount(node.right, nodeX) + 1;
     }
     
     private int countNodes(TreeNode node) {
@@ -51,3 +43,58 @@ class Solution {
     
     
 }
+
+// // O(3N) O(N) 
+// // optimization: countTop is nothing but totalNodes - (left + right)
+// class Solution {
+//     public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+//         // for node x,
+//         // get number of left subtree nodes
+//         // get number of right subtree nodes
+//         // get number of top nodes
+//         // if by cutting the biggest one of these 3, we are able to secure
+//         // more than half of the nodes, we can win
+        
+//         TreeNode nodeX = getNodeX(root, x);
+        
+//         int leftCount = countNodes(nodeX.left);
+//         int rightCount = countNodes(nodeX.right);
+        
+//         int topCount = getTopCount(root, nodeX);
+        
+//         int bestScope = Math.max(leftCount, Math.max(rightCount, topCount));
+        
+//         int possibleScope = leftCount + rightCount + topCount + 1 - bestScope;
+        
+//         return possibleScope < (n/2) + 1;
+//     }
+    
+//     private int getTopCount(TreeNode node, TreeNode nodeX) {
+//         if (node == null) return 0;
+        
+//         if (node == nodeX) return 0;
+        
+//         return getTopCount(node.left, nodeX) + getTopCount(node.right, nodeX) + 1;
+//     }
+    
+//     private int countNodes(TreeNode node) {
+//         if (node == null) return 0;
+//         return countNodes(node.left) + countNodes(node.right) + 1;
+//     }
+    
+//     private TreeNode getNodeX(TreeNode node, int x) {
+//         if (node == null) return null;
+        
+//         if (node.val == x) return node;
+        
+//         TreeNode leftSearch = getNodeX(node.left, x);
+//         if (leftSearch != null) return leftSearch;
+        
+//         TreeNode rightSearch = getNodeX(node.right, x);
+//         if (rightSearch != null) return rightSearch;
+        
+//         return null;
+//     }
+    
+    
+// }
