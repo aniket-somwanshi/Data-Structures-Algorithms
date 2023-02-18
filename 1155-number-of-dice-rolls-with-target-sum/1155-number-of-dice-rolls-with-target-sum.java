0e78c6cbd39d4ac3d1e5f12d450580f0262fa63f
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-02-18 13:31:59 +0530

    Time: 18 ms (71.67%), Space: 41 MB (88.00%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,26 +1,54 @@
+// O(target*N * K) O(target*N) -- memo
 class Solution {
-    int MOD = 1000000000 + 7;
-    Map<String, Integer> memo = new HashMap<>();
-    public int numRollsToTarget(int d, int f, int target) {
-        if (d == 0 && target == 0) {
-            return 1;
+    private int MOD = 1000000007;
+    private int k;
+    
+    private Integer[][] memo;
+    public int numRollsToTarget(int n, int k, int target) {
+        this.k = k;
+        this.memo = new Integer[target+1][n+1];
+        return f(target, n);
+    }
+    
+    private int f(int t, int n) {
+        if (n == 0) {
+           return t == 0 ? 1 : 0;
+        }  
+        if (t < 0) return 0;
+        
+        if (memo[t][n] != null) return memo[t][n];
+        
+        int totalPossibleWays = 0;
+        // select any number on the dice upto k
+        for (int dice = 1; dice <= k; dice++) {
+            totalPossibleWays = ((f(t - dice, n-1) % MOD) + totalPossibleWays) % MOD;
         }
-        if (d == 0 || target == 0) {
-            return 0;
-        }
-        String str = d + " " + target;
-        if (memo.containsKey(str)) {
-            return memo.get(str);
-        }
-        int res = 0;
-        for (int i = 1; i <= f; i++) {
-            if (target >= i) {
-                res = (res + numRollsToTarget(d - 1, f, target - i)) % MOD;
-            } else {
-                break;
-            }
-        }
-        memo.put(str, res);
-        return res;
+             
+        return memo[t][n] = totalPossibleWays;
     }
 }
+
+// // O(K^N) O(N) -- brute
+// class Solution {
+//     private int MOD = 1000000007;
+//     private int k;
+    
+//     public int numRollsToTarget(int n, int k, int target) {
+//         this.k = k;
+//         return f(target, n);
+//     }
+    
+//     private int f(int t, int n) {
+//         if (n == 0) {
+//            return t == 0 ? 1 : 0;
+//         }  
+        
+//         int totalPossibleWays = 0;
+//         // select any number on the dice upto k
+//         for (int dice = 1; dice <= k; dice++) {
+//             totalPossibleWays = ((f(t - dice, n-1) % MOD) + totalPossibleWays) % MOD;
+//         }
+        
+//         return totalPossibleWays;
+//     }
+// }
