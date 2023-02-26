0c3945d5ca0e5b8fcb409df7844c4a7f8d9f31c3
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-02-26 13:49:25 +0530

    Time: 37 ms (87.33%), Space: 52.4 MB (35.75%) - LeetHub

---
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        List<String> res = new LinkedList<>();
+        
+        Set<String> set = new HashSet<>();
+        
+        Arrays.sort(folder, (a,b)->a.length()-b.length());
+        
+        for(String s: folder) {
+            boolean isSubFolder = false;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '/') {
+                    // we have found a prefix path
+                    // see if this path is already present in set
+                    if (set.contains(sb.toString())) {
+                        isSubFolder = true;
+                        break;
+                    }
+                    sb.append(s.charAt(i)); // '/'
+                }   
+                else {
+                    sb.append(s.charAt(i));
+                }
+            }
+            if (!isSubFolder) {
+                res.add(s);
+                set.add(s);
+            }
+        }
+        
+        return res;
+    }
+}
