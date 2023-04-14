95ca60a1a21dc0cda3cdc17ecb7afe67083e9bfc
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-04-14 10:56:49 +0530

    Time: 43 ms (85.93%), Space: 44.5 MB (86.69%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.java
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.java
@@ -1,16 +1,15 @@
+// O(N) O(N)
 class Solution {
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
         int maxiOccurrences = 0;
         Map<String, Integer> map = new HashMap<>(); // string, occurrences
         int n = s.length();
         
-        for (int i = 0; i < n; i++) {
-            for (int j = i + minSize - 1; j < Math.min(n, i + maxSize); j++) {
-                String subString = s.substring(i, j+1);
-                if (isValid(subString, maxLetters)) {
-                    map.put(subString, map.getOrDefault(subString, 0) + 1);
-                    maxiOccurrences = Math.max(maxiOccurrences, map.get(subString));
-                }
+        for (int i = 0; i < n - minSize + 1 ; i++) {
+            String subString = s.substring(i, i + minSize);
+            if (isValid(subString, maxLetters)) {
+                map.put(subString, map.getOrDefault(subString, 0) + 1);
+                maxiOccurrences = Math.max(maxiOccurrences, map.get(subString));
             }
         }
         
@@ -27,3 +26,34 @@ class Solution {
         return true;
     }
 }
+
+// // O(N*N) O(N)
+// class Solution {
+//     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
+//         int maxiOccurrences = 0;
+//         Map<String, Integer> map = new HashMap<>(); // string, occurrences
+//         int n = s.length();
+        
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + minSize - 1; j < Math.min(n, i + maxSize); j++) {
+//                 String subString = s.substring(i, j+1);
+//                 if (isValid(subString, maxLetters)) {
+//                     map.put(subString, map.getOrDefault(subString, 0) + 1);
+//                     maxiOccurrences = Math.max(maxiOccurrences, map.get(subString));
+//                 }
+//             }
+//         }
+        
+//         return maxiOccurrences;
+//     }
+    
+//     private boolean isValid(String s, int maxLetters) {
+//         int[] count = new int[26];
+//         for (char c: s.toCharArray()) {
+//             count[c-'a']++;
+//             maxLetters -= count[c-'a'] == 1 ? 1 : 0;
+//             if (maxLetters < 0) return false;
+//         }
+//         return true;
+//     }
+// }
