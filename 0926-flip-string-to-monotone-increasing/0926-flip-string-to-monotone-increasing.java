7d9415f95924972b1c7b31f3de0b6ca8974fed3f
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-05-07 13:05:11 +0530

    Time: 136 ms (5.07%), Space: 77.2 MB (5.64%) - LeetHub

---
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -0,0 +1,97 @@
+// O(N*2) O(N*2 + N) memo
+class Solution {
+    char[] a;
+    int n;
+    Integer[][] memo;
+    public int minFlipsMonoIncr(String s) {
+        a = s.toCharArray();
+        n = a.length;
+        memo = new Integer[n][2];
+        return f(0, false);
+    }
+    
+    private int f(int i, boolean mustBeOne) {
+        if (i == n) return 0;
+        
+        int res = Integer.MAX_VALUE;
+        
+        if (memo[i][mustBeOne ? 0 : 1] != null) return memo[i][mustBeOne ? 0 : 1];
+        
+        if (mustBeOne) {
+            if (a[i] == '0') {
+                // it's not matching the rule, so we must flip it
+                res = Math.min(res, 1 + f(i+1, mustBeOne));
+            }
+            else {
+                // it should be a 1 and it is a 1 so no problem
+                res = Math.min(res, f(i+1, mustBeOne));
+            }
+        }
+        
+        if (!mustBeOne) {
+            if (a[i] == '0') {
+                // it can be a zero, so it's just fine
+                res = Math.min(res, f(i+1, mustBeOne));
+            }
+            else {
+                // it can be a zero, but it's a one
+                
+                // case 1: it's a normal transition from 0s to 1s
+                res = Math.min(res, f(i+1, true));
+                
+                // case 2: we want to continue the zeros streak, 
+                // so we make this into a zero, and continue that
+                res = Math.min(res, 1 + f(i+1, false));
+            }
+        }
+        
+        return memo[i][mustBeOne ? 0 : 1] = res;
+    }
+}
+
+// Recursion: O(N^3 + N) O(N)
+// class Solution {
+//     char[] a;
+//     int n;
+//     public int minFlipsMonoIncr(String s) {
+//         a = s.toCharArray();
+//         n = a.length;
+//         return f(0, false);
+//     }
+    
+//     private int f(int i, boolean mustBeOne) {
+//         if (i == n) return 0;
+        
+//         int res = Integer.MAX_VALUE;
+        
+//         if (mustBeOne) {
+//             if (a[i] == '0') {
+//                 // it's not matching the rule, so we must flip it
+//                 res = Math.min(res, 1 + f(i+1, mustBeOne));
+//             }
+//             else {
+//                 // it should be a 1 and it is a 1 so no problem
+//                 res = Math.min(res, f(i+1, mustBeOne));
+//             }
+//         }
+        
+//         if (!mustBeOne) {
+//             if (a[i] == '0') {
+//                 // it can be a zero, so it's just fine
+//                 res = Math.min(res, f(i+1, mustBeOne));
+//             }
+//             else {
+//                 // it can be a zero, but it's a one
+                
+//                 // case 1: it's a normal transition from 0s to 1s
+//                 res = Math.min(res, f(i+1, true));
+                
+//                 // case 2: we want to continue the zeros streak, 
+//                 // so we make this into a zero, and continue that
+//                 res = Math.min(res, 1 + f(i+1, false));
+//             }
+//         }
+        
+//         return res;
+//     }
+// }
