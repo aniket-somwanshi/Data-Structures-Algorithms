273544530e1f243fa703e847e07f0ebe05252471
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-05-08 23:27:55 +0530

    Time: 39 ms (37.72%), Space: 45 MB (14.37%) - LeetHub

--- 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.java
+++ 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.java
@@ -1,38 +1,66 @@
-// O(N*N) O(N + N) memo
+// O(N*N) O(N + N) greedy
+// we should try to fit as much more numbers as possible in each parition!
 class Solution {
-    char[] a;
-    int n;
-    int k;
-    Integer[] memo;
     public int minimumPartition(String s, int k) {
-        a = s.toCharArray();
-        n = a.length;
-        this.k = k;
-        memo = new Integer[n];
-        int res = f(0);
-        return res == Integer.MAX_VALUE ? -1 : res;
-    }
-    
-    private int f(int ind) {
-        if (ind == n) return 0;
+        char[] a = s.toCharArray();
+        int n = a.length;
         
-        if (memo[ind] != null) return memo[ind];
-        
-        int miniPartitions = Integer.MAX_VALUE;
+        int totalPartitions = 0;
         long currentNumber = 0;
-        for (int i = ind; i < n; i++) {
-            currentNumber = (currentNumber * 10) + Integer.parseInt(String.valueOf(a[i]));
-            if (currentNumber <= k) {
-                int res = f(i+1);
-                if (res != Integer.MAX_VALUE) miniPartitions = Math.min(miniPartitions, 1 + res);
+        int i = 0;
+        while (i < n) {
+            int startOfPartition = i;
+            while (i < n && (currentNumber * 10) + Integer.parseInt(String.valueOf(a[i])) <= k) {
+                currentNumber = (currentNumber * 10) + Integer.parseInt(String.valueOf(a[i]));
+                i++;
             }
-            else break;
+            if (i > startOfPartition)  {
+                totalPartitions++;
+                currentNumber = 0;
+            }
+            else return -1;
         }
-        return memo[ind] = miniPartitions;
+        return totalPartitions;
     }
     
     
 }
+
+// // O(N*N) O(N + N) memo
+// class Solution {
+//     char[] a;
+//     int n;
+//     int k;
+//     Integer[] memo;
+//     public int minimumPartition(String s, int k) {
+//         a = s.toCharArray();
+//         n = a.length;
+//         this.k = k;
+//         memo = new Integer[n];
+//         int res = f(0);
+//         return res == Integer.MAX_VALUE ? -1 : res;
+//     }
+    
+//     private int f(int ind) {
+//         if (ind == n) return 0;
+        
+//         if (memo[ind] != null) return memo[ind];
+        
+//         int miniPartitions = Integer.MAX_VALUE;
+//         long currentNumber = 0;
+//         for (int i = ind; i < n; i++) {
+//             currentNumber = (currentNumber * 10) + Integer.parseInt(String.valueOf(a[i]));
+//             if (currentNumber <= k) {
+//                 int res = f(i+1);
+//                 if (res != Integer.MAX_VALUE) miniPartitions = Math.min(miniPartitions, 1 + res);
+//             }
+//             else break;
+//         }
+//         return memo[ind] = miniPartitions;
+//     }
+    
+    
+// }
 
 // O((N^N)*N) O(N)
 // class Solution {
