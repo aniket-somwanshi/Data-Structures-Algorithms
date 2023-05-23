ecfd7bf09e94d3ead95533ae969f6e306746c4b9
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-05-23 22:41:44 +0530

    Time: 19 ms (43.75%), Space: 80 MB (96.88%) - LeetHub

---
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int countWays(int[][] ranges) {
+        int n = ranges.length;
+        Arrays.sort(ranges, (a,b)->a[0]-b[0]);
+        int endPoint = ranges[0][1];
+        int distincts = 1;
+        for (int i = 1; i < n; i++) {
+            if (ranges[i][0] > endPoint) {
+                distincts++;
+            }
+            endPoint = Math.max(endPoint, ranges[i][1]);
+        }
+        // int res = (int)(0L + (long)Math.pow(2, distincts) % 1000000007);
+        System.out.println(distincts);
+        int a=1;
+        for(int i=0;i<distincts;i++){
+            a=(int)((1L * a*2)% 1000000007);
+        }
+        return a;
+    }
+    int MOD = 1000000007;
+  private int calcPower(int base, int power)  //modulo exponentation
+  {
+      long ans = 1;
+      while(power > 0)
+      {
+          if (power % 2 != 0) 
+          {
+              ans = (ans * (long)base) % MOD; 
+              power--; 
+          }
+          else 
+          {
+              power = power / 2;
+              base = (int)(base * (long)base) % MOD;
+          }
+      }
+      return (int)ans;
+  }
+}
