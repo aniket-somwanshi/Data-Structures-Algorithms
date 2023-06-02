2cf8770ac312a9d9a68935bbdec0121e0791cee4
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-06-02 22:49:20 +0530

    Time: 4 ms (99.22%), Space: 43.8 MB (6.25%) - LeetHub

---
+++ 0467-unique-substrings-in-wraparound-string/0467-unique-substrings-in-wraparound-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int findSubstringInWraproundString(String s) {
+        int n = s.length();
+        char[] a = s.toCharArray();
+        int[] prefix = new int[n];
+        prefix[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (a[i] == 'a' && a[i-1] == 'z') {
+                prefix[i] = prefix[i-1] + 1;
+            }
+            else if ((int)a[i] - (int)a[i-1] == 1) {
+                prefix[i] = prefix[i-1] + 1;
+            }
+            else prefix[i] = 1;
+        }
+        
+        int res = 0;
+        int[] freq = new int[26];
+        for (int i = 0; i < n; i++) {
+            int previous = freq[a[i]-'a'];
+            int current = prefix[i];
+            
+            if (current > previous) {
+                res += current - previous; 
+                freq[a[i]-'a'] = current;
+            }
+            
+        }
+        return res;
+    }
+}
