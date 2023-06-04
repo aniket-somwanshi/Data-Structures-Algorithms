10ca78680901943b6afc57e4412e7c3665b4aab5
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-06-04 23:36:02 +0530

    Time: 11 ms (39.78%), Space: 66.6 MB (24.73%) - LeetHub

---
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.java
@@ -0,0 +1,47 @@
+class Solution {
+    Integer[][] memo;
+    int[][] a;
+    int[][] mc;
+    int rows;
+    int cols;
+    public int minPathCost(int[][] a, int[][] mc) {
+        rows = a.length;
+        cols = a[0].length;
+        
+        this.a = a;
+        this.mc = mc;
+        
+        // memo[i][j] is minimum cost of path
+        // starting from [i][j] and 
+        // ending at the last row
+        memo = new Integer[rows][cols];
+        
+        int mini = Integer.MAX_VALUE;
+        for (int j = 0; j < cols; j++) {
+            mini = Math.min(mini, miniCost(0, j));
+        }
+        
+        return mini;
+    }
+    
+    private int miniCost(int i, int j) {
+        // base case
+        if (i == rows - 1) return a[i][j];
+        
+        if (memo[i][j] != null) return memo[i][j];
+        
+        int cost = Integer.MAX_VALUE;
+        
+        for (int nextJ = 0; nextJ < cols; nextJ++) {
+            int currentValue = a[i][j];
+            int movingCost = mc[currentValue][nextJ];
+            int currentCost = movingCost +  
+                miniCost(i+1, nextJ);
+            cost = Math.min(cost, currentCost);
+        }
+        
+        cost += a[i][j];
+        
+        return memo[i][j] = cost;
+    }
+}
