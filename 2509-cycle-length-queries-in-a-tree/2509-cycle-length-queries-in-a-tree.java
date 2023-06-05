459c63b3a02a6ee535ce5b97666093843d6137bc
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-06-05 13:31:24 +0530

    Time: 718 ms (10.26%), Space: 89.4 MB (32.05%) - LeetHub

---
+++ 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] cycleLengthQueries(int n, int[][] queries) {
+        int m = queries.length;
+        int[] res = new int[m];
+        for (int i = 0; i < m; i++) {
+            int a = queries[i][0];
+            int b = queries[i][1];
+            int lca = getLCA(a, b);
+            int LCAtoA = log2(a) - log2(lca);
+            int LCAtoB = log2(b) - log2(lca);
+            int answer = LCAtoA + LCAtoB + 1;
+            res[i] = answer;
+        }
+        return res;
+    }
+    
+    private int getLCA(int a, int b) {
+        while (a != b) {
+            if (log2(a) > log2(b)) {
+                a = a/2;
+            }
+            else if (log2(b) > log2(a)) {
+                b = b/2;
+            }
+            else a = a/2; 
+        }
+        return a; // or b
+    }
+    
+    private int log2(int a) {
+        // log(a) base b = log10(a)/log10(b) 
+        // we want base 2 so b =2
+        // log(a) base 2 = log10(a)/log10(2)
+        return (int)(Math.log(a)/Math.log(2));
+    }
+    
+}
