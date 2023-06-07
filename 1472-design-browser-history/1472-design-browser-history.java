dee8eb85175104320979b6944f0f9bbeee4f37b3
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-06-07 23:28:54 +0530

    Time: 59 ms (68.00%), Space: 50.2 MB (72.68%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -0,0 +1,37 @@
+class BrowserHistory {
+    int currentIndex;
+    int currentSize;
+    Map<Integer, String> map;
+    public BrowserHistory(String homepage) {
+        currentIndex = 1;
+        currentSize = 1;
+        map = new HashMap<>();
+        map.put(currentIndex, homepage);
+    }
+    
+    public void visit(String url) {
+        currentIndex++;
+        map.put(currentIndex, url);
+        currentSize = currentIndex;
+    }
+    
+    public String back(int steps) {
+        int targetIndex = Math.max(1, currentIndex - steps);
+        currentIndex = targetIndex;
+        return map.get(currentIndex);
+    }
+    
+    public String forward(int steps) {
+        int targetIndex = Math.min(currentSize, currentIndex + steps);
+        currentIndex = targetIndex;
+        return map.get(currentIndex);
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
