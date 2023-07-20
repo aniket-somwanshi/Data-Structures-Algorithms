df5f56731bc64a74e8f585d256897b1581b260f9
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-07-20 14:46:20 +0530

    Time: 6 ms (17.60%), Space: 44.7 MB (75.87%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,22 +1,11 @@
-// O(log(M+N)) O(1)
 class Solution {
     public double findMedianSortedArrays(int[] a1, int[] a2) {
         int n = a1.length;
         int m = a2.length;
         
         if (m < n) return findMedianSortedArrays(a2, a1);
-        
-//         if (n == 0) return m%2 == 1 ? (double)a2[(m/2)] : (double)(a2[m/2] + a2[(m/2)-1]) / 2.0;
-//         if (m == 0) return n%2 == 1 ? (double)a1[(n/2)] : (double)(a1[n/2] + a1[(n/2)-1]) / 2.0;
-        
-        // try to take 'k' elements from a1
-        
-        // l1 -> end index of a1 in left
-        // l2 -> end index of a2 in left
-        // r1 -> start index of a1 in right
-        // r2 -> start index of a2 in right
         int low = 0;
-        int high = n;
+        int high = Math.min(n, (m+n+1)/2);
         
         while (low <= high) {
             int mid = low + (high - low)/2;
@@ -30,34 +19,30 @@ class Solution {
             int r1 = k1 == n ? Integer.MAX_VALUE : a1[k1];
             int r2 = k2 == m ? Integer.MAX_VALUE : a2[k2];
             
-            // check to see if it's valid or left heavy or right heavy
-            // We know that a1[l1] should be <= a2[r2]
-            // So if it's not, it means a1's l1 holds bigger elements than required
-            // how to make l1 have smaller element? 
-            // we know l1 means last el of a1 taken. And the array is sorted
-            // so reduce the number of el taken from a1
-            // ie reduce k, ie mid
-            if (l1 > r2) {
-                high = mid-1;
-            }
-            else if (l2 > r1) {
-                // the opposite case where a2 has too many big elements, 
-                // so decrease it by increasing k ie mid
-                low = mid+1;
-            }
-            else {
-                // this means it's a valid partition, so we found the median elements
-                // return the median based on odd or even lengths
-                if ((m+n)%2==1) {
-                    // odd
-                    return (double)Math.max(l1, l2); 
+            
+            if (l1 <= r2 && l2 <= r1) {
+                // return ans
+                if ((n+m) % 2 == 1) {
+                    System.out.println(mid);
+                    // System.out.println(a2[l2]);
+                    
+                    // biggest in the left half
+                    return Math.max(l1, l2); 
                 }
                 else {
-                    // even
-                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0; 
+                    int candidate1 =  Math.max(l1, l2);
+                    int candidate2 = Math.min(r1, r2);
+                    return (candidate1 + candidate2)/2.00;
                 }
             }
-        } 
-        return 0.0;
+            else if (l1 > r2) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return -1;
     }
+    
 }
