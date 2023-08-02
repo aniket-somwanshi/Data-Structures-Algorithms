567b066fefa506822fae1cba63894f8e0c9e44e9
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-02 20:50:08 +0530

    Time: 20 ms (87.29%), Space: 44.5 MB (27.20%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,29 +1,27 @@
+// O(N^2) O(N)
 class Solution {
     public String longestPalindrome(String s) {
-        for (int length = s.length(); length > 0; length--) {
-            for (int start = 0; start <= s.length() - length; start++) {
-                if (check(start, start + length, s)) {
-                    return s.substring(start, start + length);
-                }
-            }
+        int n = s.length();
+        // if the longest palindrom is of size 1,
+        // it can definitely be a[0]
+        // so this becomes our base answer
+        String[] res = new String[1];
+        res[0] = String.valueOf(s.charAt(0));
+        for (int i = 0; i < n; i++) {
+            expandFromCenter(i, i, s, res);
+            expandFromCenter(i, i+1, s, res);
         }
-        
-        return "";
+        return res[0];
     }
     
-    private boolean check(int i, int j, String s) {
-        int left = i;
-        int right = j - 1;
-        
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
+    private void expandFromCenter(int i, int j, String s, String[] res) {
+        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+            if (j-i+1 > res[0].length()) {
+                // System.out.println(res);
+                res[0] = s.substring(i,j+1);
             }
-            
-            left++;
-            right--;
+            i--;
+            j++;
         }
-        
-        return true;
     }
 }
