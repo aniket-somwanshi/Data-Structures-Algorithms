e5bf0f0470dc498e5f899f7311fc9d925169e2e9
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-04 00:12:41 +0530

    Time: 153 ms (14.25%), Space: 43 MB (96.92%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,64 +1,35 @@
-// O(klogN) O(1) -- N = all the nodes overall
-// divide and conquer(merge)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        int n = lists.length;
-        if (n == 0) return null;
-        int interval = 1;
-        while (interval < n) {
-            for (int i = 0; i + interval < n; i= i + interval*2) {
-                lists[i] = merge2Lists(lists[i], lists[i+interval]);
-            }
-            interval *= 2;
-        }
-        return lists[0];
-    }
-    
-    private ListNode merge2Lists(ListNode node1, ListNode node2) {
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
+        ListNode preHead = new ListNode();
+        ListNode res = preHead;
         
-        while (node1 != null && node2 != null) {
-            if (node1.val <= node2.val) {
-                current.next = node1;
-                current = current.next;
-                node1 = node1.next;
+        while (true) {
+            // find the smallest donny
+            int smallestIndex = -1;
+            for (int i = 0; i < lists.length; i++) {
+                if (lists[i] != null && (smallestIndex == -1 || lists[i].val < lists[smallestIndex].val)) {
+                    smallestIndex = i;
+                }
             }
-            else {
-                current.next = node2;
-                current = current.next;
-                node2 = node2.next;
+            if (smallestIndex == -1) {
+                // means there is legit no one left
+                break;
             }
+            res.next = lists[smallestIndex];
+            lists[smallestIndex] = lists[smallestIndex].next;
+            res = res.next;
         }
         
-        if (node1 == null) current.next = node2;
-        if (node2 == null) current.next = node1;
-        
-        return dummy.next;
+        return preHead.next;
     }
 }
-
-
-// // O(2NlogN) O(N) -- N = all the nodes overall
-// Heap solution
-// class Solution {
-//     public ListNode mergeKLists(ListNode[] lists) {
-//         PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)->a.val-b.val);
-        
-//         for (ListNode node: lists) {
-//             while (node != null) {
-//                 pq.add(node);
-//                 node = node.next;
-//             }
-//         }
-        
-//         ListNode dummy = new ListNode(-1);
-//         ListNode current = dummy;
-//         while (!pq.isEmpty()) {
-//             current.next = pq.poll();
-//             current = current.next;
-//         }
-//         current.next = null;
-//         return dummy.next;
-//     }
-// }
