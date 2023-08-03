7e18b7683b0a881633c90562e42afbca69a77e47
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-03 20:32:01 +0530

    Time: 31 ms (96.44%), Space: 51.2 MB (42.77%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,39 +1,37 @@
-// O(NlogN + N*N) O(N)
-// indice i, j, k should not have duplicate values
-// eg. i should not be equal to i-1 as it's already computed for
-// and j should not be equal to j-1  
 class Solution {
-    public List<List<Integer>> threeSum(int[] a) {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        int n = a.length;
-        Arrays.sort(a);
-        
-        
-        for (int i = 0; i < n; i++) {
-            // do not repeat a triplet value that is already considered
-            if (i > 0 && a[i] == a[i-1]) continue;
-            
-            int target = -a[i];
-            
-            // find pair that sums to target
+        int i = 0;
+        while (i < n) {
+            int target = -nums[i];
             int j = i+1;
             int k = n-1;
             
             while (j < k) {
-                if (a[j] + a[k] == target) {
-                    res.add(new ArrayList<>(Arrays.asList(a[i], a[j], a[k])));
+                int sum = nums[j] + nums[k];
+                
+                if (sum < target) {
                     j++;
-                    while (j < k && a[j] == a[j-1]) j++;
-                    k--;
-                    while (k > j && a[k] == a[k+1]) k--;
                 }
-                else if (a[j] + a[k] > target) {
+                else if (sum > target) {
                     k--;
-                }
+                }  
                 else {
-                    j++;
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                    res.add(triplet);
+                    // skip duplicates
+                    while (j < k && nums[j] == triplet.get(1)) {
+                        j++;
+                    }
+                    while (k > j && nums[k] == triplet.get(2)) {
+                        k--;
+                    }
                 }
-            } 
+            }
+            i++;
+            while (i < n && nums[i] == nums[i-1]) i++;
         }
         
         return res;
