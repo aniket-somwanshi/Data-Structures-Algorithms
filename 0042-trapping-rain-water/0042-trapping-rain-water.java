b0d08ec77f8db6d1ec7502ba578c428532101fe4
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-04 15:58:04 +0530

    Time: 1 ms (92.97%), Space: 45 MB (8.89%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,57 +1,32 @@
 class Solution {
-    /*
-    O(N) O(1)
-    Put i on left and j on right end
-    we calculate the outer container that can contain some water
-    Assuming there are no buildngs inside it, there will be all water
-    So this is our inintial water amt
-    Now since there are buildings inside this boundary, 
-    If we get a building, we should decrease the amt of water, 
-    as the space is actually occupied by the building
-    If we get i and j such that building-i and building j-i can become
-    containers of larger height that the current containers level,
-    we will increase our max possible water amt by the 
-    increased height*distance between them
-    For moving i and j, we say always move the smaller height pointer
-    keep moving until we cover all the buildings
-    */
-    public int trap(int[] a) {
+    public int trap(int[] a) { 
         int n = a.length;
-        int i = 0;
-        int j = n-1;
-        int currentLevel = 0;
-        int totalWater = 0;
+        int i = 0; // left
+        int j = n-1; // right
+        int cmh = 0; // currentMaxHeight
+        int ca = 0; // currentArea
+        
         while (i < j) {
-            // if we find a bigger container, increase the max possible water
-            // by the additional height * distance between them
-            int newPossibleLevel = Math.min(a[i], a[j]);
-            if (newPossibleLevel > currentLevel) {
-                int increasedLevel = newPossibleLevel - currentLevel;
-                int distanceBetweenThem = j - i - 1;
-                totalWater += increasedLevel * distanceBetweenThem;
-                currentLevel = newPossibleLevel; // update
+            int potentialNewHeight = Math.min(a[i], a[j]);
+            int extraHeight = 0;
+            if (potentialNewHeight > cmh) {
+                extraHeight = potentialNewHeight - cmh;
+                cmh = potentialNewHeight;
             }
+            ca += extraHeight * (j-i-1);
             
-            // move the smaller building,
-            // cos we need the bigger building, so that we can
-            // find a bigger container later
             if (a[i] <= a[j]) {
-                i++;
-                // this is a building, so shouldnt have considered this as water
-                // so reduce whatever area of this building is from the totalWater
-                // we have considered all water only upto the `currentlevel`
-                // so only reduce currentLevel height one time, if it's >= currentLevel 
+                 i++;
                 if (i >= j) break;
-                int falseWater = Math.min(currentLevel, a[i]);
-                totalWater -= falseWater;
-            }
+                ca -= Math.min(a[i], cmh);
+            } 
             else {
                 j--;
                 if (i >= j) break;
-                int falseWater = Math.min(currentLevel, a[j]);
-                totalWater -= falseWater;
+                ca -= Math.min(a[j], cmh);
             }
         }
-        return totalWater;
+        
+        return ca;
     }
 }
