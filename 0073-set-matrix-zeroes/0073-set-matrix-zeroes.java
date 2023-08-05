9e9dbe4eaf15fbe16596833c713ee09e755d7bf1
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-06 01:28:38 +0530

    Time: 1 ms (78.26%), Space: 44.6 MB (78.01%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,41 @@
+class Solution {
+    // O(3(M+N) O(1)
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        boolean isFirstColumnZero = false;
+        for (int i = 0; i < n; i++) {
+            
+            if (matrix[i][0] == 0) {
+                isFirstColumnZero = true;
+            }
+            
+            
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0; matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+            }
+        }
+        
+        if (matrix[0][0] == 0) {
+          for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }  
+        }
+        
+        
+        if (isFirstColumnZero) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }  
+        }
+        
+    }
+}
