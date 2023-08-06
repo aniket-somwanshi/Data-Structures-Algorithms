98ec5191efbc7eca8abcb4412efb466ae0321f45
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-06 12:27:34 +0530

    Time: 1 ms (70.70%), Space: 41.5 MB (97.86%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,22 @@
+class Solution {
+    int[] a;
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        a = nums;
+        List<Integer> current = new ArrayList<>();
+        getAllSubsets(0, current, res);
+        return res;
+    }
+    
+    private void getAllSubsets(int index, List<Integer> current, List<List<Integer>> res) {        
+        res.add(new ArrayList<>(current));
+        
+        if (index == a.length) return;
+        
+        for (int i = index; i < a.length; i++) {
+            current.add(a[i]);
+            getAllSubsets(i+1, current, res);
+            current.remove(current.size()-1);
+        }
+    }
+}
