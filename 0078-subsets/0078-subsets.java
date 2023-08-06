b382005099aaca9806abba145e98c644d9a744d9
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-06 12:44:26 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (73.60%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -9,14 +9,39 @@ class Solution {
     }
     
     private void getAllSubsets(int index, List<Integer> current, List<List<Integer>> res) {        
-        res.add(new ArrayList<>(current));
-        
-        if (index == a.length) return;
-        
-        for (int i = index; i < a.length; i++) {
-            current.add(a[i]);
-            getAllSubsets(i+1, current, res);
-            current.remove(current.size()-1);
+        if (index == a.length) {
+            res.add(new ArrayList<>(current));
+            return;
         }
+        
+        // choose and not choose
+        getAllSubsets(index+1, current, res);
+        
+        current.add(a[index]);
+        getAllSubsets(index+1, current, res);
+        current.remove(current.size()-1);
     }
 }
+
+// class Solution {
+//     int[] a;
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         a = nums;
+//         List<Integer> current = new ArrayList<>();
+//         getAllSubsets(0, current, res);
+//         return res;
+//     }
+    
+//     private void getAllSubsets(int index, List<Integer> current, List<List<Integer>> res) {        
+//         res.add(new ArrayList<>(current));
+        
+//         if (index == a.length) return;
+        
+//         for (int i = index; i < a.length; i++) {
+//             current.add(a[i]);
+//             getAllSubsets(i+1, current, res);
+//             current.remove(current.size()-1);
+//         }
+//     }
+// }
