b7356bc2ea1e7c6cbe12c9461077f628615710b8
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-06 10:38:38 +0530

    Time: 0 ms (100.00%), Space: 44.9 MB (13.66%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,41 +1,43 @@
 class Solution {
-    // O(3(M+N) O(1)
-    public void setZeroes(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        boolean isFirstColumnZero = false;
-        for (int i = 0; i < n; i++) {
-            
-            if (matrix[i][0] == 0) {
-                isFirstColumnZero = true;
-            }
-            
-            
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0; matrix[0][j] = 0;
-                }
-            }
-        }
+    public void setZeroes(int[][] a) {
+        int rows = a.length;
+        int cols = a[0].length;
         
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+        boolean isFirstRowMarked = false;
+        boolean isFirstColMarked = false;
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (a[i][j] != 0) continue;
+                
+                // if this the first row or column
+                if (i == 0) isFirstRowMarked = true;
+                if (j == 0) isFirstColMarked = true;
+                
+                // mark the row & col as marked
+                a[i][0] = 0;
+                a[0][j] = 0;
             }
         }
         
-        if (matrix[0][0] == 0) {
-          for (int j = 0; j < m; j++) {
-                matrix[0][j] = 0;
-            }  
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (a[i][0] == 0 || a[0][j] == 0) a[i][j] = 0;
+            }
         }
         
-        
-        if (isFirstColumnZero) {
-            for (int i = 0; i < n; i++) {
-                matrix[i][0] = 0;
-            }  
+        if (isFirstRowMarked) {
+            for (int j = 0; j < cols; j++) {
+                a[0][j] = 0;
+            }
         }
+        
+        if (isFirstColMarked) {
+            for (int i = 0; i < rows; i++) {
+                a[i][0] = 0;
+            }
+        }
+        
         
     }
 }
