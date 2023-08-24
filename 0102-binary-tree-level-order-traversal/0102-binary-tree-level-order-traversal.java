51dba7a07a5a287a54c9cb418870d305974bb206
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-24 20:08:22 +0530

    Time: 1 ms (95.08%), Space: 43.7 MB (93.57%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,44 +1,25 @@
-// O(N) O(H) -- DFS
 class Solution {
+    Map<Integer, List<Integer>> map;
+    List<List<Integer>> res;
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new LinkedList<>();
-        if (root == null) return res;
-        dfs(root, res, 0);
+        res = new ArrayList<>();
+        map = new HashMap<>();
+        dfs(root, 0);
+        int size = map.size();
+        for (int i = 0; i < size; i++) {
+            res.add(map.get(i));
+        }
         return res;
     }
     
-    private void dfs(TreeNode node, List<List<Integer>> res, int level) {
+    private void dfs(TreeNode node, int depth) {
         if (node == null) return;
-        while (res.size() <= level) {
-            res.add(new LinkedList<>());
-        }
-        res.get(level).add(node.val);
         
-        dfs(node.left, res, level+1);
-        dfs(node.right, res, level+1);
+        dfs(node.left, depth+1);
+        
+        if (!map.containsKey(depth)) map.put(depth, new ArrayList<>());
+        map.get(depth).add(node.val);
+        
+        dfs(node.right, depth+1);
     }
 }
-
-// // O(N) O(B) -- BFS
-// class Solution {
-//     public List<List<Integer>> levelOrder(TreeNode root) {
-//         List<List<Integer>> res = new LinkedList<>();
-//         if (root == null) return res;
-        
-//         Queue<TreeNode> level = new LinkedList<>();
-//         level.add(root);
-        
-//         while (!level.isEmpty()) {
-//             int size = level.size();
-//             List<Integer> currLevel = new LinkedList<>();
-//             for (int i = 0; i < size; i++) {
-//                 TreeNode c = level.poll();
-//                 currLevel.add(c.val);
-//                 if (c.left != null) level.add(c.left);
-//                 if (c.right != null) level.add(c.right);
-//             }
-//             res.add(currLevel);
-//         }
-//         return res;
-//     }
-// }
