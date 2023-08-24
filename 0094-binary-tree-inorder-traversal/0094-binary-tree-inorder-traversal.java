c8059fb0e51647deeb69fbd311d2626d7f85ae66
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-24 15:13:01 +0530

    Time: 1 ms (8.45%), Space: 40.5 MB (89.57%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,51 +1,25 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        if (root == null) return res;
-        Stack<TreeNode> s = new Stack<>();
-        s.push(root);
-        while (!s.isEmpty()) {
-            if (s.peek() != null) {
-                TreeNode current = s.pop();
-                s.push(current.right);
-                s.push(current);
-                s.push(current.left);
-            }
-            else {
-                s.pop();
-                if (s.isEmpty()) break;
-                res.add(s.pop().val);
-            }
-        }
-        
+        Stack<TreeNode> st = new Stack<>();
+        populateLeftBoundary(st, root);
+        populateInorder(st, res);
         return res;
     }
+    
+    private void populateInorder(Stack<TreeNode> st, List<Integer> res) {
+        while (!st.isEmpty()) {
+            TreeNode current = st.pop();
+            res.add(current.val);
+            populateLeftBoundary(st, current.right);
+        }
+    }
+    
+    private void populateLeftBoundary(Stack<TreeNode> st, TreeNode current) {
+        while (current != null) {
+            st.push(current);
+            current = current.left;
+        }
+    }
+    
 }
-
-
-
-
-
-// class Solution {
-//     public List<Integer> inorderTraversal(TreeNode root) {
-//         // O(N), O(height)
-//         List<Integer> res = new ArrayList<>();
-//         if (root == null) return res;
-        
-//         Stack<TreeNode> stack = new Stack<>();
-//         TreeNode node = root;
-//         while(true) {
-//             if (node != null) {
-//                 stack.push(node);
-//                 node = node.left;
-//             }
-//             else if (node == null) {
-//                 if (stack.isEmpty()) break; 
-//                 TreeNode leftDone = stack.pop();
-//                 res.add(leftDone.val);
-//                 node = leftDone.right;
-//             }
-//         }
-//         return res;
-//     }
-// }
