8973616d029a3c44a8a9aa57ca7bc8041d91f3b8
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-24 19:57:29 +0530

    Time: 1 ms (12.05%), Space: 40.5 MB (78.39%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,17 +1,32 @@
-// O(N) O(H) -- dfs
+// O(N) O(N) -- iterative
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return isValid(root.left, root.right);
-    }
-    
-    private boolean isValid(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null) return true;
-        if (node1 == null) return false;
-        if (node2 == null) return false;
+        if (root == null) return true;
         
-        if (node1.val != node2.val) return false;
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
         
-        return isValid(node1.left, node2.right) && 
-            isValid(node1.right, node2.left);
+        q1.add(root.left);
+        q2.add(root.right);
+        
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            TreeNode n1 = q1.poll();
+            TreeNode n2 = q2.poll();
+            
+            if ((n1 == null && n2 != null) || (n1 != null && n2 == null)) return false;
+            
+            if (n1 != null) {
+                // (Note: n2 is never null here, based on above checks)
+                if (n1.val != n2.val) return false;
+                
+                q1.add(n1.left);
+                q1.add(n1.right);
+                
+                q2.add(n2.right);
+                q2.add(n2.left);
+            }
+        }
+        
+        return true;
     }
 }
