ed9f3db7f941f16e5cc501dce0231d7da7661e9d
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-24 23:39:50 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (54.63%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -1,28 +1,51 @@
-// O(N) O(N)
+// O(N) O(N) -- recursive
 class Solution {
     public Node connect(Node root) {
-        if (root == null) return root;
+        util(root);
+        return root;
+    }
+    
+    private void util(Node node) {
+        if (node == null) return;
         
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        
-        while (!q.isEmpty()) {
-            int size = q.size();
-            Node prev = null;
-            while (size-- > 0) {
-                Node node = q.poll();
-                if (prev != null) {
-                    prev.next = node;
-                }
-                prev = node;
-                
-                // add children
-                if (node.left != null) q.add(node.left);
-                if (node.right != null) q.add(node.right);
-                
+        if (node.left != null) {
+            node.left.next = node.right;
+            
+            if (node.next != null) {
+                node.right.next = node.next.left; 
             }
         }
         
-        return root;
+        util(node.left);
+        util(node.right);
     }
 }
+
+// // O(N) O(N) -- iterative
+// class Solution {
+//     public Node connect(Node root) {
+//         if (root == null) return root;
+        
+//         Queue<Node> q = new LinkedList<>();
+//         q.add(root);
+        
+//         while (!q.isEmpty()) {
+//             int size = q.size();
+//             Node prev = null;
+//             while (size-- > 0) {
+//                 Node node = q.poll();
+//                 if (prev != null) {
+//                     prev.next = node;
+//                 }
+//                 prev = node;
+                
+//                 // add children
+//                 if (node.left != null) q.add(node.left);
+//                 if (node.right != null) q.add(node.right);
+                
+//             }
+//         }
+        
+//         return root;
+//     }
+// }
