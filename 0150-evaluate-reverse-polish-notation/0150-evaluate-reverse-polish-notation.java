106fb7d7c6c6b203b644e3780eb345318ce4ecf6
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-26 22:38:06 +0530

    Time: 6 ms (88.53%), Space: 43.1 MB (81.89%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,41 +1,38 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        
-        Stack<Integer> operands = new Stack();          //Declaring empty stack
-        
-        int a = -1, b = -1;                        //operand variables; 
-        for(int i=0;i<tokens.length;i++) {
-		
-		   //If the tokens[i] is equals to any of the operand then pop two recently added values from stack and perform the given operation
-		   
-            if(tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/")) {
-                a =  operands.pop();
-                b =  operands.pop();
-                
-                if(tokens[i].equals("+"))          //If operator equals to '+'
-                     operands.push(a + b);
-					 
-                else if(tokens[i].equals("-"))     //If operator equals to '-'
-                     operands.push(b - a);
-					 
-                else if(tokens[i].equals("*"))     //If operator equals to '*'
-                    operands.push(a * b);
-					
-                else                               //If operator equals to '/'
-                     operands.push(b / a);
+        Stack<Integer> st = new Stack<>();
+        for (String s: tokens) {
+            if (s.equals("+")) {
+                // notice the chronology, num2 is second num cos if LIFO
+                // this matters in divison, subtraction
+                int num2 = st.pop(); 
+                int num1 = st.pop(); 
+                int res = num1 + num2;
+                st.push(res);
             }
-			
-			//If the tokens[i] not equals to any of the operator then it must be number i.e. operand so adding it to the stack
-            else {
-                 operands.push(Integer.parseInt(tokens[i]));
+            else if (s.equals("-")) {
+                int num2 = st.pop(); 
+                int num1 = st.pop(); 
+                int res = num1 - num2;
+                st.push(res);
             }
-                
+            else if (s.equals("*")) {
+                int num2 = st.pop(); 
+                int num1 = st.pop(); 
+                int res = num1 * num2;
+                st.push(res);
+            }
+            else if (s.equals("/")) {
+                int num2 = st.pop(); 
+                int num1 = st.pop(); 
+                int res = num1 / num2;
+                st.push(res);
+            }
+            else { 
+                int num = Integer.parseInt(s);
+                st.push(num);
+            }
         }
-        
-		//The final answer will be stored in the stack on top, so returning the popped element
-        return operands.pop();
-        
+        return st.pop();
     }
 }
-
-
