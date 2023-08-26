2d19ac11675c05e22281451542bb83de8f725381
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-26 16:01:56 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (55.07%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -1,55 +1,26 @@
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-// O(N) O(1)
 class Solution {
     public Node copyRandomList(Node head) {
-        Node dummy = new Node(-1);
-        dummy.next = head;
+        Node ogNode = new Node(-1);
+        ogNode.next = head;
         
         Map<Node, Node> map = new HashMap<>();
+        Node newNode = new Node(-1);
         
-        Node ogNode = head;
         while (ogNode != null) {
-            if (!map.containsKey(ogNode)) {
-                // put the newNode in map
-                Node newNode = new Node(ogNode.val);
-                map.put(ogNode, newNode);
-            }
-            Node newNode = map.get(ogNode);
-            
-            // process it's random node
-            if (ogNode.random != null && !map.containsKey(ogNode.random)) {
-                // put new random node in map
-                Node newRandomNode = new Node(ogNode.random.val);
-                map.put(ogNode.random, newRandomNode);
-            }
-            // assign random node
-            if (ogNode.random != null) newNode.random = map.get(ogNode.random);
-            
-            // process it's next node
             if (ogNode.next != null && !map.containsKey(ogNode.next)) {
-                Node newNextNode = new Node(ogNode.next.val);
-                map.put(ogNode.next, newNextNode);
+                map.put(ogNode.next, new Node(ogNode.next.val));
             }
-            // assign next node
-            if (ogNode.next != null) newNode.next = map.get(ogNode.next);
+            newNode.next = map.get(ogNode.next);
             
-            // move forward 
+            if (ogNode.random != null && !map.containsKey(ogNode.random)) {
+                map.put(ogNode.random, new Node(ogNode.random.val));
+            }
+            newNode.random = map.get(ogNode.random);
+            
+            newNode = newNode.next;
             ogNode = ogNode.next;
         }
-        
-        return map.get(dummy.next);
+                
+        return map.get(head);
     }
 }
