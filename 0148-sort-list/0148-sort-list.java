e1da520130a49981729407e10b590a51e0fb5502
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-08-26 19:53:28 +0530

    Time: 11 ms (62.13%), Space: 56.7 MB (11.64%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,70 @@
+class Solution {
+    public ListNode sortList(ListNode node) {
+        if (node == null) return null;
+        if (node.next == null) return node;
+            
+        // divide
+        ListNode midNode = getMid(node);
+        
+        ListNode firstUnsorted = node;
+        ListNode secondUnsorted = midNode.next;
+        
+        // break the link
+        midNode.next = null;
+        
+        // further divide them 
+        ListNode first = sortList(firstUnsorted);
+        ListNode second = sortList(secondUnsorted);
+        
+        // merge the 2 sorted donnies
+        return getSortedList(first, second);
+    }
+    
+    // O(N+M) O(1)
+    private ListNode getSortedList(ListNode first, ListNode second) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode current = dummyHead;
+        
+        while (first != null && second != null) {
+            if (first.val < second.val) {
+                current.next = first;
+                first = first.next;
+            }
+            else {
+                current.next = second;
+                second = second.next;
+            }
+            current = current.next;
+        }
+        
+        while (first != null) {
+            current.next = first;
+            first = first.next;
+            current = current.next;
+        }
+        
+        while (second != null) {
+            current.next = second;
+            second = second.next;
+            current = current.next;
+        }
+        
+        return dummyHead.next;
+    }
+    
+    private ListNode getMid(ListNode node) {
+        // if there are only 2 donnies, return first one
+        if (node.next.next == null) return node;
+        
+        // when fast pointer reaches the end, slow pointer will reach the mid
+        ListNode fast = node.next;
+        ListNode slow = node;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+}
