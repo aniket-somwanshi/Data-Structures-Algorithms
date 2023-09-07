4c07f321e287a207e84fef22bb393b74f529cb4b
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-09-07 12:21:51 +0530

    Time: 2 ms (53.23%), Space: 43.1 MB (85.60%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,26 +1,19 @@
-// O(N) O(1) 
-// a high product can be obtained by
-// 1. multiplying a high +ve no by a +ve no
-// 2. multiplying a high -ve no by a -ve no
-// so keep running max and mins
-// and try multiplying them with current el 
-// to get a possibly best product
 class Solution {
-    public int maxProduct(int[] a) {
-        int miniCurrent = a[0];
-        int maxiCurrent = a[0];
+    public int maxProduct(int[] nums) {
+        int maxi = nums[0];
+        int mini = nums[0];
+        int res = nums[0];
         
-        int res = a[0];
-        
-        for (int i = 1; i < a.length; i++) {
-            int maxiCurrentOG = maxiCurrent;
-            int miniCurrentOG = miniCurrent;
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
             
-            maxiCurrent = Math.max(a[i], Math.max(maxiCurrentOG * a[i], miniCurrentOG * a[i]));
+            int newMaxi = Math.max(maxi * num, Math.max(mini * num, num));
+            int newMini = Math.min(mini * num, Math.min(maxi * num, num));
             
-            miniCurrent = Math.min(a[i], Math.min(maxiCurrentOG * a[i], miniCurrentOG * a[i]));
+            maxi = newMaxi;
+            mini = newMini;
             
-            res = Math.max(res, maxiCurrent);
+            res = Math.max(res, maxi);
         }
         
         return res;
