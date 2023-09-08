4c233e14062c0b94d8999d0347fd87d45f32bf6b
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-09-08 11:59:09 +0530

    Time: 6 ms (41.38%), Space: 43.7 MB (11.80%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.java
@@ -0,0 +1,57 @@
+class Solution {
+    public String largestNumber(int[] a) {
+        int n = a.length;
+        // convert all numbers to strings
+        String[] s = new String[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = String.valueOf(a[i]);
+        }
+        Arrays.sort(s, (x,y)->cmp(x,y));
+        String res= String.join("", s);
+        // remove leading zeroes
+        int i = 0;
+        while (i < res.length() && res.charAt(i) == '0') i++;
+        if (i == res.length()) return "0";
+        return res.substring(i);
+    }
+    
+    // returns non-positive value if n1 should come before n2
+    // returns non-positive value if n1 > n2
+    private int cmp(String n1, String n2) {
+        String n12 = n1+n2;
+        String n21 = n2+n1;
+        for (int i = 0; i < n1.length()+n2.length(); i++) {
+            if (n12.charAt(i) > n21.charAt(i)) return -1;
+            else if (n21.charAt(i) > n12.charAt(i)) return 1;
+        }
+        return 0;
+//         int i = 0;
+//         int j = 0;
+//         int miniSize = Math.min(n1.length(), n2.length());
+        
+//         while (i < miniSize && j < miniSize) {
+//             if (n1.charAt(i) > n2.charAt(j)) {
+//                 return -1;
+//             }
+//             else if (n2.charAt(j) > n1.charAt(i)) {
+//                 return 1;
+//             }
+//             i++;
+//             j++;
+//         }
+        
+//         // here, one of the number is finished
+//         if (i < n1.length()) { // n2 has finished
+//             // n1 is greater than n2 only if the current digit of n1 
+//             // is greater than the previous common digit 
+//             if (n1.charAt(i) > n1.charAt(i-1)) return -1;
+//             else return 1;
+//         }
+//         else if (j < n2.length()) {
+//             if (n2.charAt(j) > n2.charAt(j-1)) return 1;
+//             else return -1;
+//         }
+        
+//         return 0;
+    }
+}
