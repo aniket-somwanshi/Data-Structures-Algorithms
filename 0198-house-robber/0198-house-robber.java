52cff1abf19a2d75315b6ebfacb19c4a94bf8ec3
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-09-08 23:40:12 +0530

    Time: 0 ms (100.00%), Space: 40 MB (33.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,34 @@
+// O(N) O(N+N)
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        Integer[] memo = new Integer[n];
-        return f(0, nums, memo);
+        int[] dp = new int[n+2];
+        
+        // base case
+        dp[n] = 0;
+        dp[n+1] = 0;
+        
+        // explore
+        for (int i = n-1; i>=0; i--) {
+            dp[i] = Math.max(nums[i] + dp[i+2], dp[i+1]);
+        }
+        
+        return dp[0];
     }
     
-    private int f(int i, int[] nums, Integer[] memo) {
-        if (i >= nums.length) return 0;
-        if (memo[i] != null) return memo[i];
-        return memo[i] = Math.max(nums[i] + f(i+2, nums, memo), f(i+1, nums, memo));
-    }
 }
+
+// // O(N) O(N+N)
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         Integer[] memo = new Integer[n];
+//         return f(0, nums, memo);
+//     }
+    
+//     private int f(int i, int[] nums, Integer[] memo) {
+//         if (i >= nums.length) return 0;
+//         if (memo[i] != null) return memo[i];
+//         return memo[i] = Math.max(nums[i] + f(i+2, nums, memo), f(i+1, nums, memo));
+//     }
+// }
