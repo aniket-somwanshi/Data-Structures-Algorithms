e2980d4974ab32360f05243d6e3845ab6cfd90bc
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-09-08 23:36:56 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (79.62%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,13 @@
-// O(N) O(1) dp
 class Solution {
-    public int rob(int[] a) {
-        if (a.length == 1) return a[0];
-        int secondLast = a[0];
-        int last = Math.max(a[0], a[1]);
-        
-        for (int i = 2; i < a.length; i++) {
-            int current = Math.max(
-                a[i] + secondLast, 
-                last
-            );
-            secondLast = last;
-            last = current;
-        }
-        return last;
+    public int rob(int[] nums) {
+        int n = nums.length;
+        Integer[] memo = new Integer[n];
+        return f(0, nums, memo);
+    }
+    
+    private int f(int i, int[] nums, Integer[] memo) {
+        if (i >= nums.length) return 0;
+        if (memo[i] != null) return memo[i];
+        return memo[i] = Math.max(nums[i] + f(i+2, nums, memo), f(i+1, nums, memo));
     }
 }
