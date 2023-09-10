a0f51f7788b52cf8bf62b6851030733b15d2dcfd
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-09-10 18:57:19 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (70.46%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -1,17 +1,38 @@
-// recursive 
+// true recursive 
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        return head == null ? null : f(head, null);
-    }
-    
-    private ListNode f(ListNode current, ListNode prev) {
-        ListNode nextNode = current.next;
-        current.next = prev;
-        prev = current;
-        current = nextNode;
-        return current == null ? prev : f(current, prev);
+    public ListNode reverseList(ListNode current) {
+        if (current == null) return null;
+        
+        // if this is actually the last one in the original list then
+        // this will be our `lastReversedHead` which we should return ultimately
+        ListNode lastReversedHead = current; 
+        
+        // if this is not the last node to be reversed, then
+        // we first recurse to the last, get `lastReversedHead` assigned 
+        //to the last node, and then reverse it
+        if (current.next != null) {
+            lastReversedHead = reverseList(current.next);
+            current.next.next = current; // reversed list's head should point to the current 
+        }
+        current.next = null; 
+        return lastReversedHead;
     }
 }
+
+// // recursive 
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         return head == null ? null : f(head, null);
+//     }
+    
+//     private ListNode f(ListNode current, ListNode prev) {
+//         ListNode nextNode = current.next;
+//         current.next = prev;
+//         prev = current;
+//         current = nextNode;
+//         return current == null ? prev : f(current, prev);
+//     }
+// }
 
 // // iterative 
 // class Solution {
