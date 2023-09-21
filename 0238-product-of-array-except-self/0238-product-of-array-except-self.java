ce89b3ab2c9a7093b61193e5268016a782895aac
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-09-21 23:38:43 +0530

    Time: 3 ms (13.87%), Space: 52.9 MB (46.60%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,22 +1,44 @@
-// O(N) O(N)
+// O(N) O(1)
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] prefix = new int[n];
-        int[] suffix = new int[n];
         int[] res = new int[n];
+        Arrays.fill(res, 1);
 
-        for (int i = 0; i < n; i++) {
-            int j = n-i-1;
-            prefix[i] = nums[i] * (i-1 >= 0 ? prefix[i-1] : 1); 
-            suffix[j] = nums[j] * (j+1 < n ? suffix[j+1] : 1); 
+        int prefixProduct = 1;
+        for (int i = 1; i < n; i++) {
+            prefixProduct *= nums[i-1];
+            res[i] *= prefixProduct;
         }
         
-        for (int i = 0; i < n; i++) {
-            int prev = i-1 >= 0 ? prefix[i-1] : 1;
-            int next = i+1 < n ? suffix[i+1] : 1;
-            res[i] = prev * next;
+        int suffixProduct = 1;
+        for (int i = n-2; i >= 0; i--) {
+            suffixProduct *= nums[i+1];
+            res[i] *= suffixProduct;
         }
         return res;
     }
 }
+
+// // O(N) O(N)
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int n = nums.length;
+//         int[] prefix = new int[n];
+//         int[] suffix = new int[n];
+//         int[] res = new int[n];
+
+//         for (int i = 0; i < n; i++) {
+//             int j = n-i-1;
+//             prefix[i] = nums[i] * (i-1 >= 0 ? prefix[i-1] : 1); 
+//             suffix[j] = nums[j] * (j+1 < n ? suffix[j+1] : 1); 
+//         }
+        
+//         for (int i = 0; i < n; i++) {
+//             int prev = i-1 >= 0 ? prefix[i-1] : 1;
+//             int next = i+1 < n ? suffix[i+1] : 1;
+//             res[i] = prev * next;
+//         }
+//         return res;
+//     }
+// }
