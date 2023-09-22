cc7d464e5472a121168119d3488a5ed340c0bbae
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-09-22 17:40:40 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (13.77%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,9 +1,22 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int exp = n*(n+1)/2;
+        
+        int exp = 0;
+        for (int i = 0; i <= n; i++) exp ^= i;
+        
         int act = 0;
-        for (int num: nums) act += num;
-        return (int)(exp-act);
+        for (int num: nums) act ^= num;
+        
+        return exp ^ act;
     }
 }
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int n = nums.length;
+//         long exp = n*(n+1)/2;
+//         long act = 0;
+//         for (int num: nums) act += num;
+//         return (int)(exp-act);
+//     }
+// }
