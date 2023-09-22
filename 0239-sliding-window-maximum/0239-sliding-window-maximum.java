8a46284712e1e928d5925944ae768d54314ea4df
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-09-22 13:29:14 +0530

    Time: 26 ms (94.93%), Space: 59.5 MB (52.78%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,54 +1,40 @@
 class Solution {
     public int[] maxSlidingWindow(int[] a, int k) {
         int n = a.length;
-        int[] output = new int[(n-k)+1];
-        int outputIndex = 0;
-        
-        int left = 0;
-        int right = 0;
-        
-        Deque<Integer> dq = new LinkedList<>(); // stores indices
-        
-        while (right < n) {
-            // before inserting a[right], make sure the 
-            // useless values that are lesser than it,
-            // from the right side of the dq, are removed
-            // cos they will never be a max of any window anymore
-            // as, to their right, we have this bigger value
-            // which is a candidate for being the max 
-            while (!dq.isEmpty() && a[dq.peekLast()] < a[right]) {
+        int[] res = new int[n-k+1];
+        Deque<Integer> dq = new ArrayDeque<>(); // Deque<Index>
+        // calculate the first window manually
+        for (int i = 0; i < k; i++) {
+            while (!dq.isEmpty() && a[dq.peekLast()] < a[i]) {
                 dq.pollLast();
             }
+            dq.addLast(i);
+        }
+        // we maintain the dq in monotonically decreasing order
+        // max element in the window is always the first element in dq
+        res[0] = a[dq.peekFirst()];
+        
+        for (int i = 1; i < n-k+1; i++) {
+            // one element just went out of bounds so remove it from left
+            // if it's there at all
+            if (dq.peekFirst() < i) dq.pollFirst();
             
-            // now we can insert the value at right
-            dq.addLast(right);
-            
-            // if we are shifting our window forward to the right,
-            // left index will no longer be in consideration innit
-            // so if in our dq(which represents our window)
-            // if we have an index at the start which is no longer in window,
-            // remove it
-            if (dq.peekFirst() < left) {
-                dq.pollFirst();
+            int newElementIndex = i+k-1;
+            // insert the new element, to make sure the mono decreasing
+            // state is maintained, remove anyone who's lesser than the current
+            // element from the end of queue
+            // We can remove those cos they'd never contribute to max going forward
+            // cos the current superior element will cockblock them innit
+            while (!dq.isEmpty() && a[dq.peekLast()] < a[newElementIndex]) {
+                dq.pollLast();
             }
-            
-            // if we have processed at least k elements,
-            // this is our window
-            if (right + 1 >= k) { // +1 for 0-based indices
-                // for this window what is our max
-                // it's the leftmost guy in the dq
-                // as we have maintained the dq to be decreasing order
-                // so get it and put in the output
-                output[outputIndex++] = a[dq.peekFirst()];
-                
-                // here, we know that we had a k sized window,
-                // so move the window forward
-                left++;
-            }
-            // move right every step, even thought we don't have k elements yet
-            right++;
+            dq.addLast(newElementIndex);
+            // all the elements in dq are that in the valid window boundary
+            // all the elements in dq are decreasing order
+            // so the first element is the current window's max
+            res[i] = a[dq.peekFirst()];
         }
         
-        return output;
+        return res;
     }
 }
