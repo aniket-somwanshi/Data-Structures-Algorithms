582c43513f5540af9b02297fd6ed919f7a4f2709
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-09-23 18:31:27 +0530

    Time: 4 ms (84.89%), Space: 56.4 MB (62.34%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,24 +1,46 @@
+// O(N) O(1)
 class Solution {
     public int findDuplicate(int[] nums) {
         int n = nums.length-1;
-        int low = 1;
-        int high = n;
         
-        while (low < high) {
-            int mid = low + (high-low)/2;
-            
-            int c = 0;
-            for (int num: nums) {
-                if (num <= mid) c++;
-            }
-            
-            if (c > mid) {
-                high = mid;
-            }
-            else {
-                low = mid + 1;
-            }
+        int slow = 0;
+        int fast = 0;
+        
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return low;
+        return slow;
     }
 }
+// // O(NlogN) O(1)
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         int n = nums.length-1;
+//         int low = 1;
+//         int high = n;
+        
+//         while (low < high) {
+//             int mid = low + (high-low)/2;
+            
+//             int c = 0;
+//             for (int num: nums) {
+//                 if (num <= mid) c++;
+//             }
+            
+//             if (c > mid) {
+//                 high = mid;
+//             }
+//             else {
+//                 low = mid + 1;
+//             }
+//         }
+//         return low;
+//     }
+// }
