3e1e13025c75c76936a2530ad9222bc5b96bc0b6
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-09-30 23:32:48 +0530

    Time: 6 ms (85.63%), Space: 42.7 MB (95.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,130 +1,37 @@
-// // O(NlogN) O(N) --binary search
-// class Solution {
-//     public int lengthOfLIS(int[] a) {
-//         int n = a.length;
-//         int[] res = new int[n];
-//         int lastIndex = 0;
-//         res[lastIndex++] = a[0];
-//         for (int i = 1; i < n; i++) {
-//             if (a[i] > res[lastIndex-1]) {
-//                 res[lastIndex++] = a[i];
-//             }
-//             else {
-//                 int position = getPosition(0, lastIndex-1, res, a[i]);
-//                 res[position] = a[i];
-//             }
-//         }
-//         return lastIndex;
-//     }
+// O(NlogN) O(N)
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        List<Integer> sub = new ArrayList<>(); // increasing subsequence
+        int maxi = 1;
+        for (int num: nums) {
+            if (sub.isEmpty() || sub.get(sub.size()-1) < num) {
+                sub.add(num);
+            }
+            else {
+                replaceWithFirstElementGreaterThan(num, sub);
+            }
+            maxi = Math.max(maxi, sub.size());
+        }
+        return maxi;
+    }
     
-//     private int getPosition(int start, int end, int[] a, int target) {
-//         while (start <= end) {
-//             int mid = start + (end - start)/2;
-//             if (a[mid] == target) return mid;
-//             else if (target > a[mid]) start = mid + 1;
-//             else end = mid - 1;
-//         }
-//         return start;
-//     }
-// }
-
-// O(N*N) O(N*N) --dp (finding max LIS ending at each index)
-// this is useful for printing the actual LIS
-class Solution {    
-    public int lengthOfLIS(int[] a) {
-        int n = a.length;
-        int[] LISEndingAt = new int[n];
-        Arrays.fill(LISEndingAt, 1);
-        
-        for (int i = 0; i < n; i++) {
-            for (int prev = 0; prev < i; prev++) {
-                if (a[prev] < a[i]) LISEndingAt[i] = Math.max(LISEndingAt[i], LISEndingAt[prev] + 1);
+    private void replaceWithFirstElementGreaterThan(int num, List<Integer> sub) {
+        // binary search to find the smallest element greater than num
+        int left = 0;
+        int right = sub.size()-1;
+        int res = -1;
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            if (sub.get(mid) >= num) {
+                res = mid;
+                right = mid-1;
+            }
+            else {
+                left = mid + 1;
             }
         }
-        int res = 0;
-        for (int candidate: LISEndingAt) res = Math.max(res, candidate);
-        return res;
+        
+        sub.set(res, num);
     }
 }
-
-// class Solution {
-//     // O(N*N) O(N*N) --memo
-//     // pick the current element if it extends current LIS,
-//     // also skip current element
-//     public int lengthOfLIS(int[] a) {
-//         int n = a.length;
-//         Integer[][] memo = new Integer[n][n];
-        
-//         return recur(0, -1, a, memo);
-//     }
-    
-//     private int recur(int i, int prevIndex, int[] a, Integer[][] memo) {
-//         if (i == a.length) return 0;
-        
-//         if (prevIndex != -1 && memo[i][prevIndex] != null) return memo[i][prevIndex];  
-        
-//         int take = 0;
-//         // take if possible  
-//         if (prevIndex == -1 || a[i] > a[prevIndex]) {
-//             take = 1 + recur(i+1, i, a, memo);
-//         }
-        
-//         // not take
-//         int notTake = recur(i+1, prevIndex, a, memo);
-        
-//         int res = Math.max(take, notTake);
-//         if (prevIndex != -1) memo[i][prevIndex] = res;
-//         return res;
-//     }
-//} 
-
-
-// class Solution {
-//     // O(N^2) O(N)
-//     // DP
-//     // Start from starting index to see if it extend previous lcs
-//     public int lengthOfLIS(int[] a) {
-//         int n = a.length;
-//         int[] dp = new int[n];
-//         Arrays.fill(dp, 1);
-//         int maxi = 1;
-//         dp[n-1] = 1;
-//         for (int i = n-2; i >= 0; i--) {
-//             for (int j = i+1; j < n; j++) {
-//                 if (a[j] > a[i]) {
-//                     dp[i] = Math.max(dp[i], 1 + dp[j]);
-//                 }
-//             }
-//             maxi = Math.max(maxi, dp[i]);
-//         }
-//         return maxi;
-//     }
-// } 
-    // O(N^2) O(N)
-    // Recursion backtracking
-    // memoization
-    // cache stores length of longest inc subsequence starting at i
-//     public int lengthOfLIS(int[] input) {
-//         int max = Integer.MIN_VALUE;
-//         int [] cache = new int [input.length];
-//         for (int i = 0; i < input.length; ++i) {
-//             max = Math.max(max, lengthOfLIS(input,i, cache));
-//         }
-//         return Math.max(1,max);
-//     } 
-
-//     private static int lengthOfLIS(int[] input, int start, int [] cache) {
-
-//         if (start >= input.length) return 0;
-//         if (cache[start] != 0) return cache[start];
-
-//         int max = Integer.MIN_VALUE;
-//         for (int i = start; i < input.length; ++i) {
-//             if (input[i] > input[start]) {
-//                 max = Math.max(max,1 + lengthOfLIS(input, i, cache));
-//             }
-//         }
-//         cache[start] = Math.max(max,1);
-//         return cache[start];
-//     }
-// }
