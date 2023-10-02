0e6be568f4db980de4394975a77114646d934982
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-10-02 18:07:34 +0530

    Time: 28 ms (50.92%), Space: 89.3 MB (67.71%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,36 +1,44 @@
 class RandomizedSet {
     List<Integer> list;
-    HashMap<Integer, Integer> valToIndex;
-    Random random;
+    Map<Integer, Integer> map;
+    java.util.Random rand = new java.util.Random();
     public RandomizedSet() {
         list = new ArrayList<>();
-        valToIndex = new HashMap<>();
-        random = new Random();
+        map = new HashMap<>();
     }
     
     public boolean insert(int val) {
-        if(valToIndex.containsKey(val)){
-            return false;
-        }
+        if (map.containsKey(val)) return false;
+
         list.add(val);
-        valToIndex.put(val, list.size()-1);
+        int insertedIndex = list.size()-1;
+        map.put(val, insertedIndex);
         return true;
     }
     
     public boolean remove(int val) {
-        if(!valToIndex.containsKey(val)){
-            return false;
+        if (!map.containsKey(val)) return false;
+        int indexToRemove = map.get(val);
+        
+        if (indexToRemove < list.size()-1) {
+            int lastNum = list.get(list.size()-1);
+            list.set(indexToRemove, lastNum);
+            map.put(lastNum, indexToRemove);
         }
-        int index = valToIndex.get(val);
-        int last = list.get(list.size() - 1);
-        list.set(index, last);
-        list.remove(list.size() - 1);
-        valToIndex.put(last, index);
-        valToIndex.remove(val);
+        map.remove(val);
+        list.remove(list.size()-1);
         return true;
     }
     
     public int getRandom() {
-        return list.get(random.nextInt(list.size()));
+        return list.get((int)rand.nextInt(list.size()));
     }
 }
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
