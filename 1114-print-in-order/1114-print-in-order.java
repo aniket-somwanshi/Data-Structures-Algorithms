940bd37d337ea26ef2a50421591333e0a67d0197
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-10-10 16:16:40 +0530

    Time: 11 ms (85.64%), Space: 41.4 MB (61.62%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.java
+++ 1114-print-in-order/1114-print-in-order.java
@@ -1,29 +1,53 @@
-// using semaphores
+// using volatie 
 class Foo {
-    private Semaphore semaphore1;
-    private Semaphore semaphore2;
+    private volatile int count = 0;
     public Foo() {
-        semaphore1 = new Semaphore(0);
-        semaphore2 = new Semaphore(0);
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
         printFirst.run();
-        semaphore1.release();
+        count++;
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        semaphore1.acquire();
+        while (count <= 0) {}
         printSecond.run();
-        semaphore2.release();
+        count++;
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        semaphore2.acquire();
+        while (count <= 1) {}
         printThird.run();
-        semaphore2.release();
+        count++;
     }
 }
+
+// // using semaphores
+// class Foo {
+//     private Semaphore semaphore1;
+//     private Semaphore semaphore2;
+//     public Foo() {
+//         semaphore1 = new Semaphore(0);
+//         semaphore2 = new Semaphore(0);
+//     }
+
+//     public void first(Runnable printFirst) throws InterruptedException {
+//         printFirst.run();
+//         semaphore1.release();
+//     }
+
+//     public void second(Runnable printSecond) throws InterruptedException {
+//         semaphore1.acquire();
+//         printSecond.run();
+//         semaphore2.release();
+//     }
+
+//     public void third(Runnable printThird) throws InterruptedException {
+//         semaphore2.acquire();
+//         printThird.run();
+//         semaphore2.release();
+//     }
+// }
 // using lock and notify wait
 // class Foo {
 //     private Object lock;
