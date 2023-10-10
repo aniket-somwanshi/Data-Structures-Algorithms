f427998d6a0b44b8e55cdffe8e736fa0fd7afe9c
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-10-10 16:15:23 +0530

    Time: 12 ms (53.73%), Space: 41.7 MB (25.66%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.java
+++ 1114-print-in-order/1114-print-in-order.java
@@ -1,39 +1,65 @@
+// using semaphores
 class Foo {
-    private Object lock;
-    boolean oneDone;
-    boolean twoDone;
+    private Semaphore semaphore1;
+    private Semaphore semaphore2;
     public Foo() {
-        lock = new Object();
+        semaphore1 = new Semaphore(0);
+        semaphore2 = new Semaphore(0);
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
-        
-        synchronized (lock) {
-            printFirst.run();
-            oneDone = true;
-            lock.notifyAll();
-        }
+        printFirst.run();
+        semaphore1.release();
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        synchronized (lock) {
-            while (!oneDone) {
-                lock.wait();
-            } 
-            printSecond.run();
-            twoDone = true;
-            lock.notifyAll();
-        }
-        
+        semaphore1.acquire();
+        printSecond.run();
+        semaphore2.release();
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        synchronized (lock) {
-            while (!twoDone) {
-                lock.wait();
-            }
-            printThird.run();
-        }
-        
+        semaphore2.acquire();
+        printThird.run();
+        semaphore2.release();
     }
 }
+// using lock and notify wait
+// class Foo {
+//     private Object lock;
+//     boolean oneDone;
+//     boolean twoDone;
+//     public Foo() {
+//         lock = new Object();
+//     }
+
+//     public void first(Runnable printFirst) throws InterruptedException {
+//         synchronized (lock) {
+//             printFirst.run();
+//             oneDone = true;
+//             lock.notifyAll();
+//         }
+//     }
+
+//     public void second(Runnable printSecond) throws InterruptedException {
+//         synchronized (lock) {
+//             while (!oneDone) {
+//                 lock.wait();
+//             } 
+//             printSecond.run();
+//             twoDone = true;
+//             lock.notifyAll();
+//         }
+        
+//     }
+
+//     public void third(Runnable printThird) throws InterruptedException {
+//         synchronized (lock) {
+//             while (!twoDone) {
+//                 lock.wait();
+//             }
+//             printThird.run();
+//         }
+        
+//     }
+// }
