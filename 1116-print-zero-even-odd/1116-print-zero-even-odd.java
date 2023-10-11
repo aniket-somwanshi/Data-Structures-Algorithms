3620c064bb9547b4e3830f137b784e0a757dd654
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-10-11 16:22:49 +0530

    Time: 6 ms (99.43%), Space: 41.4 MB (55.98%) - LeetHub

--- 1116-print-zero-even-odd/1116-print-zero-even-odd.java
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.java
@@ -1,68 +1,113 @@
-// semaphore
+// synchronization using one shared variable
 class ZeroEvenOdd {
     private int n;
     
-    private Semaphore semaZero = new Semaphore(1);
-    private Semaphore semaOdd = new Semaphore(1);
-    private Semaphore semaEven = new Semaphore(0);
-    
-    private boolean isZeroNeeded = true;
-    private int i = 1;
+    private int currentTurn = 0;
     
     public ZeroEvenOdd(int n) {
         this.n = n;
     }
 
     // printNumber.accept(x) outputs "x", where x is an integer.
-    public void zero(IntConsumer printNumber) throws InterruptedException {
-        while (i <= n) {
-            if (isZeroNeeded) {
-                semaZero.acquire();
-                if (i > n) {
-                    return;
-                }
-                printNumber.accept(0);
-                isZeroNeeded = false;
+    public synchronized void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            while (currentTurn != 0) {
+                wait();
             }
+            printNumber.accept(0);
+            currentTurn = i%2 == 1 ? 1 : 2;
+            notifyAll();
         }
     }
 
-    public void even(IntConsumer printNumber) throws InterruptedException {
-        while (i <= n) {
-            if (!isZeroNeeded && i%2 == 0) {
-                semaEven.acquire();
-                if (i > n) {
-                    semaOdd.release();
-                    semaZero.release();
-                    return;
-                }
-                printNumber.accept(i);
-                i++;
-                isZeroNeeded = true;
-                semaOdd.release();
-                semaZero.release();
+    public synchronized void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i+=2) {
+            while (currentTurn != 2) {
+                wait();
             }
+            printNumber.accept(i);
+            currentTurn = 0;
+            notifyAll();
         }
     }
 
-    public void odd(IntConsumer printNumber) throws InterruptedException {
-        while (i <= n) {
-            if (!isZeroNeeded && i%2 == 1) {
-                semaOdd.acquire();
-                if (i > n) {
-                    semaEven.release();
-                    semaZero.release();
-                    return;
-                }
-                printNumber.accept(i);
-                i++;
-                isZeroNeeded = true;
-                semaEven.release();
-                semaZero.release();
+    public synchronized void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i+=2) {
+            while (currentTurn != 1) {
+                wait();
             }
+            printNumber.accept(i);
+            currentTurn = 0;
+            notifyAll();
         }
     }
 }
+
+// // semaphore
+// class ZeroEvenOdd {
+//     private int n;
+    
+//     private Semaphore semaZero = new Semaphore(1);
+//     private Semaphore semaOdd = new Semaphore(1);
+//     private Semaphore semaEven = new Semaphore(0);
+    
+//     private boolean isZeroNeeded = true;
+//     private int i = 1;
+    
+//     public ZeroEvenOdd(int n) {
+//         this.n = n;
+//     }
+
+//     // printNumber.accept(x) outputs "x", where x is an integer.
+//     public void zero(IntConsumer printNumber) throws InterruptedException {
+//         while (i <= n) {
+//             if (isZeroNeeded) {
+//                 semaZero.acquire();
+//                 if (i > n) {
+//                     return;
+//                 }
+//                 printNumber.accept(0);
+//                 isZeroNeeded = false;
+//             }
+//         }
+//     }
+
+//     public void even(IntConsumer printNumber) throws InterruptedException {
+//         while (i <= n) {
+//             if (!isZeroNeeded && i%2 == 0) {
+//                 semaEven.acquire();
+//                 if (i > n) {
+//                     semaOdd.release();
+//                     semaZero.release();
+//                     return;
+//                 }
+//                 printNumber.accept(i);
+//                 i++;
+//                 isZeroNeeded = true;
+//                 semaOdd.release();
+//                 semaZero.release();
+//             }
+//         }
+//     }
+
+//     public void odd(IntConsumer printNumber) throws InterruptedException {
+//         while (i <= n) {
+//             if (!isZeroNeeded && i%2 == 1) {
+//                 semaOdd.acquire();
+//                 if (i > n) {
+//                     semaEven.release();
+//                     semaZero.release();
+//                     return;
+//                 }
+//                 printNumber.accept(i);
+//                 i++;
+//                 isZeroNeeded = true;
+//                 semaEven.release();
+//                 semaZero.release();
+//             }
+//         }
+//     }
+// }
 
 // wait notify
 // class ZeroEvenOdd {
