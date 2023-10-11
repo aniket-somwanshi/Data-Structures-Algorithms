a56b8f902f8c605a372967ce2d75a066de0db348
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-10-11 15:18:50 +0530

    Time: 23 ms (88.81%), Space: 42.4 MB (89.54%) - LeetHub

--- 1115-print-foobar-alternately/1115-print-foobar-alternately.java
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.java
@@ -1,45 +1,72 @@
+// using semaphores
 class FooBar {
     private int n;
-    private int turn = 0; // 0 = foo, 1 = bar
-    private Object lock = new Object();
-    int c1 = 0;
-    int c2 = 0;
+    private Semaphore semaFoo = new Semaphore(1);
+    private Semaphore semaBar = new Semaphore(0);
 
     public FooBar(int n) {
         this.n = n;
     }
 
     public void foo(Runnable printFoo) throws InterruptedException {
-        
-        while (c1 < n) {
-            synchronized(lock) {
-                if (turn == 0) {
-                    printFoo.run();
-                    c1++;
-                    turn = 1;
-                    lock.notifyAll();
-                }
-                else {
-                    lock.wait();
-                }
-            }
+        for (int i = 0; i < n; i++) {
+            semaFoo.acquire();
+            printFoo.run();
+            semaBar.release();
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
-        
-         while (c2 < n) {
-            synchronized(lock) {
-                if (turn == 1) {
-                    printBar.run();
-                    c2++;
-                    turn = 0;
-                    lock.notifyAll();
-                }
-                else {
-                    lock.wait();
-                }
-            }
+       for (int i = 0; i < n; i++) {
+            semaBar.acquire();
+            printBar.run();
+            semaFoo.release();
         }
     }
 }
+// using notify wait
+// class FooBar {
+//     private int n;
+//     private int turn = 0; // 0 = foo, 1 = bar
+//     private Object lock = new Object();
+//     int c1 = 0;
+//     int c2 = 0;
+
+//     public FooBar(int n) {
+//         this.n = n;
+//     }
+
+//     public void foo(Runnable printFoo) throws InterruptedException {
+        
+//         while (c1 < n) {
+//             synchronized(lock) {
+//                 if (turn == 0) {
+//                     printFoo.run();
+//                     c1++;
+//                     turn = 1;
+//                     lock.notifyAll();
+//                 }
+//                 else {
+//                     lock.wait();
+//                 }
+//             }
+//         }
+//     }
+
+//     public void bar(Runnable printBar) throws InterruptedException {
+        
+//          while (c2 < n) {
+//             synchronized(lock) {
+//                 if (turn == 1) {
+//                     printBar.run();
+//                     c2++;
+//                     turn = 0;
+//                     lock.notifyAll();
+//                 }
+//                 else {
+//                     lock.wait();
+//                 }
+//             }
+//         }
+//     }
+// }
