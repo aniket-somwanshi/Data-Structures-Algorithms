4ed1a7eed2757f310cb5f6f4dae997a44a986c81
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-11-13 23:26:28 +0530

    Time: 5 ms (97.49%), Space: 55.3 MB (95.34%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
@@ -1,29 +1,65 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    long res = 0, total = 0, sub;
+    int m = (int)1e9 + 7;
+    long res = 0;
+    long total = 0;
     public int maxProduct(TreeNode root) {
-        total = s(root); s(root);
-        return (int)(res % (int)(1e9 + 7));
+        total = getTotal(root);
+        f(root);
+        return (int)(res % m);
     }
-
-    private long s(TreeNode root) {
-        if (root == null) return 0;
-        sub = root.val + s(root.left) + s(root.right);
-        res = Math.max(res, sub * (total - sub));
-        return sub;
+    
+    private long getTotal(TreeNode node) {
+        if (node == null) return 0;
+        return node.val + getTotal(node.left) + getTotal(node.right);
+    }
+    
+    private long f(TreeNode node) {
+        if (node == null) return 0;
+        long left = f(node.left);
+        long right = f(node.right);
+        long candidate1 = (1L)*(total - left)*(left);
+        res = Math.max(candidate1, res);
+        long candidate2 = (1L)*(total - right)*(right);
+        res = Math.max(candidate2, res);
+        
+        return node.val + left + right;
     }
 }
+
+
+// class Solution {
+//     int m = (int)1e9 + 7;
+//     long res = 0;
+//     public int maxProduct(TreeNode root) {
+//         f(root, 0);
+//         return (int)res;
+//     }
+    
+//     private long f(TreeNode node, long up) {
+//         if (node == null) return 0;
+        
+//         long curr = node.val;
+        
+//         long leftSum = f(node.left, up + curr);
+//         long rightSum = f(node.right, up + curr + leftSum);
+        
+//         leftSum = f(node.left, up + curr + rightSum);
+        
+//         res = Math.max(res, 
+//                        (int)
+//                        (1L * ((0L + up + leftSum + curr)*(0L + rightSum))) % m
+//                       );
+        
+//         res = Math.max(res, 
+//                        (int)
+//                        (1L * ((0L + up + curr + rightSum)*(0L + leftSum))) % m
+//                       );
+        
+//         res = Math.max(res, 
+//                        (int)
+//                        (1L * ((0L + curr + rightSum + leftSum)*(0L + up))) % m
+//                       );
+        
+//         return curr + leftSum + rightSum;
+//     }
+// }
