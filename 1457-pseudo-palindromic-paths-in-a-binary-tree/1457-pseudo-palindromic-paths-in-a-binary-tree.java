22fa7fd285a3e78421da6feeab86c087caea64f9
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2023-11-29 00:14:04 +0530

    Time: 15 ms (49.32%), Space: 68.9 MB (58.45%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -1,30 +1,67 @@
 class Solution {
+    private int count;
     public int pseudoPalindromicPaths (TreeNode root) {
-        int count = 0, path = 0;
-        
-        Deque<Pair<TreeNode, Integer>> stack = new ArrayDeque();
-        stack.push(new Pair(root, 0));
-        while (!stack.isEmpty()) {
-            Pair<TreeNode, Integer> p = stack.pop();
-            TreeNode node = p.getKey();
-            path = p.getValue();
-
-            if (node != null) {
-                // compute occurences of each digit 
-                // in the corresponding register
-                path = path ^ (1 << node.val);
-                // if it's a leaf check if the path is pseudo-palindromic
-                if (node.left == null && node.right == null) {
-                    // check if at most one digit has an odd frequency
-                    if ((path & (path - 1)) == 0) {
-                        ++count;
-                    }
-                } else {
-                    stack.push(new Pair(node.right, path));
-                    stack.push(new Pair(node.left, path));
-                }
-            }
-        }
+        count = 0;
+        int[] freq = new int[10];
+        dfs(root, freq);
         return count;
     }
+    
+    private boolean isPalindromic(int[] freq) {
+        int c = 0;
+        for (int val = 1; val <= 9; val++) {
+            if (freq[val]%2==1) c++;
+        }
+        return c <= 1;
+    }
+    
+    private void dfs(TreeNode node, int[] freq) {
+        if (node == null) return;
+        
+        freq[node.val]++;
+        
+        if (node.left == null && node.right == null) {
+            count += isPalindromic(freq) ? 1 : 0;
+        }
+        
+        dfs(node.left, freq);
+        dfs(node.right, freq);
+     
+        // backtrack
+        freq[node.val]--;
+    }
 }
+// MLE
+// class Solution {
+//     private int count;
+//     public int pseudoPalindromicPaths (TreeNode root) {
+//         count = 0;
+//         dfs(root, new ArrayList<>());
+//         return count;
+//     }
+    
+//     private boolean isPalindromic(List<Integer> list) {
+//         Set<Integer> set = new HashSet<>();
+//         for (int val: list) {
+//             if (set.contains(val)) set.remove(val);
+//             else set.add(val);
+//         }
+//         return set.size() <= 1;
+//     }
+    
+//     private void dfs(TreeNode node, List<Integer> list) {
+//         if (node == null) return;
+        
+//         list.add(node.val);
+        
+//         if (node.left == null && node.right == null) {
+//             // System.out.println(list);
+//             count += isPalindromic(list) ? 1 : 0;
+//             return;
+//         }
+        
+//         dfs(node.left, new ArrayList<>(list));
+//         dfs(node.right, new ArrayList<>(list));
+        
+//     }
+// }
