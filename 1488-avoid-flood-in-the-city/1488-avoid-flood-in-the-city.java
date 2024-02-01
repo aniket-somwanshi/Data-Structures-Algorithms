56efeacc9178cea140b2b5b4a4bcb516dabae38d
Aniket Somwanshi <53231464+aniket-somwanshi@users.noreply.github.com>
2024-02-01 23:51:33 +0530

    Time: 66 ms (62.50%), Space: 62.1 MB (44.85%) - LeetHub

--- 1488-avoid-flood-in-the-city/1488-avoid-flood-in-the-city.java
+++ 1488-avoid-flood-in-the-city/1488-avoid-flood-in-the-city.java
@@ -1,42 +1,28 @@
+// O(NlogN) O(N)
 class Solution {
-    public int[] avoidFlood(int[] a) {
-        int n = a.length;
-        Map<Integer, Integer> lastOccurance = new HashMap<>();
-        int[] nextOccurance = new int[n];
-        for (int i = n-1; i >= 0; i--) {
-            if (a[i] == 0) continue;
-            if (!lastOccurance.containsKey(a[i])) {
-                nextOccurance[i] = n;
-                lastOccurance.put(a[i], i);
-            }
-            else {
-                nextOccurance[i] = lastOccurance.get(a[i]);
-                lastOccurance.put(a[i], i);
-            }
-        }
-        
+    public int[] avoidFlood(int[] rains) {
+        int n = rains.length;
+        TreeSet<Integer> dryDayIndices = new TreeSet<>();
+        Map<Integer, Integer> lakeOccuranceMap = new HashMap<>();
         int[] res = new int[n];
-        PriorityQueue<Integer> pq = new PriorityQueue<>((x,y)->nextOccurance[x]-nextOccurance[y]);
-        // pq stores the indices sorted by closes nextOccurance first
-        
-        Set<Integer> visited = new HashSet<>();
+        Arrays.fill(res, 1);
         
         for (int i = 0; i < n; i++) {
-            if (a[i] == 0) {
-                // dry out the lake which is nearest next coming up
-                if (pq.isEmpty()) {
-                    res[i] = 1; // any lake for fucks sake 
-                }
-                else {
-                    int idx = pq.poll();
-                    visited.remove(a[idx]);
-                    res[i] = a[idx];
-                }
+            if (rains[i] == 0) {
+                dryDayIndices.add(i);
+            }
+            else if (lakeOccuranceMap.containsKey(rains[i])) {
+                // binary search to find if there's an dry day 
+                // after the prev occurance of this lake
+                Integer firstDryDayAfterLakeOccurance = dryDayIndices.higher(lakeOccuranceMap.get(rains[i]));
+                if (firstDryDayAfterLakeOccurance == null) return new int[] {};
+                res[firstDryDayAfterLakeOccurance] = rains[i];
+                dryDayIndices.remove(firstDryDayAfterLakeOccurance);
+                lakeOccuranceMap.put(rains[i], i);
+                res[i] = -1;
             }
             else {
-                if (visited.contains(a[i])) return new int[] {};
-                pq.add(i);
-                visited.add(a[i]);
+                lakeOccuranceMap.put(rains[i], i);
                 res[i] = -1;
             }
         }
